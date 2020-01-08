6de9f688a5002d1e534432ce8f96197e725211fd
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-01-08 10:51:41 +0000

    Update on Grasshopper References and Minor Changes

---
+++ SAM_Revit/SAM.Analytical.Revit/ToRevit/HostObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Autodesk.Revit.DB;
+
+using SAM.Geometry.Revit;
+
+namespace SAM.Analytical.Revit
+{
+    public static partial class Convert
+    {
+        public static HostObject ToRevit(this Document document, Panel panel)
+        {
+            HostObjAttributes aHostObjAttributes = document.ToRevit(panel.Construction, panel.PanelType);
+
+            throw new NotImplementedException();
+
+            //FilteredElementCollector filteredElementCollector = new FilteredElementCollector(document).OfClass(typeof(HostObjAttributes));
+
+            //BuiltInCategory builtInCategory = panelType.BuiltInCategory();
+            //if (builtInCategory != BuiltInCategory.INVALID)
+            //    filteredElementCollector.OfCategory(builtInCategory);
+
+            //foreach(HostObjAttributes hostObjAttributes in filteredElementCollector)
+            //{
+            //    string name = Core.Revit.Query.FullName(hostObjAttributes);
+            //    if (name.Equals(construction.Name))
+            //        return hostObjAttributes;
+            //}
+
+
+            //return null;
+        }
+    }
+}

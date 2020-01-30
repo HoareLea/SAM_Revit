4fc48cbe859f131d42cba56044cf6acd0dda3241
Michal Dengusiak <tassimulation@gmail.com>
2020-01-30 16:05:18 +0000

    fix push revit

--- SAM_Revit/SAM.Analytical.Revit/ToRevit/HostObjAttributes.cs
+++ SAM_Revit/SAM.Analytical.Revit/ToRevit/HostObjAttributes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 using Autodesk.Revit.DB;
 
 using SAM.Geometry.Revit;
@@ -12,19 +13,31 @@ namespace SAM.Analytical.Revit
         {
             if (construction == null)
                 return null;
-
             FilteredElementCollector filteredElementCollector = new FilteredElementCollector(document).OfClass(typeof(HostObjAttributes));
 
             BuiltInCategory builtInCategory = panelType.BuiltInCategory();
             if (builtInCategory != BuiltInCategory.INVALID)
                 filteredElementCollector.OfCategory(builtInCategory);
 
-            foreach(HostObjAttributes hostObjAttributes in filteredElementCollector)
+            foreach (HostObjAttributes hostObjAttributes in filteredElementCollector)
             {
                 string name = Core.Revit.Query.FullName(hostObjAttributes);
+                //if (string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(hostObjAttributes.Name))
+                //{
+                //    string familyName;
+                //    string typeName;
+                //    if (!Core.Revit.Query.TryGetFamilyNameAndTypeName(construction.Name, out familyName, out typeName))
+                //        continue;
 
-                    if (name.Equals(construction.Name))
-                        return hostObjAttributes;
+                //    if (string.IsNullOrWhiteSpace(typeName))
+                //        continue;
+
+                //    if(typeName.Equals(construction.Name))
+                //        return hostObjAttributes;
+                //}
+
+                if (name != null && name.Equals(construction.Name))
+                    return hostObjAttributes;
             }
 
             return null;
---
+++ SAM_Revit/SAM.Core.Revit/Query/TryGetFamilyNameAndTypeName.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.DB;
+
+namespace SAM.Core.Revit
+{
+    public static partial class Query
+    {
+        public static bool TryGetFamilyNameAndTypeName(string fullName, out string familyName, out string typeName)
+        {
+            familyName = null;
+            typeName = null;
+            
+            if (string.IsNullOrWhiteSpace(fullName))
+                return false;
+
+            string[] values = fullName.Split(':');
+            if(values.Length == 1)
+            {
+                typeName = values[0].Trim();
+            }
+            else if(values.Length == 2)
+            {
+                familyName = values[0].Trim();
+                typeName = values[1].Trim();
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}

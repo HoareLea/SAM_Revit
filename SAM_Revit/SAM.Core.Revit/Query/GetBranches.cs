182f1ee4907894b4ce08aa0fd5ba69a9162bf0e8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-02-03 00:52:23 +0000

    Debug2017 Solution Configuration added, GetBranches and GetConnectorManagers Queries added

---
+++ SAM_Revit/SAM.Core.Revit/Query/GetBranches.cs
@@ -0,0 +1,70 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+
+namespace SAM.Core.Revit
+{
+    public static partial class Query
+    {
+        public static List<List<ConnectorManager>> GetBranches(ConnectorManager connectorManager)
+        {
+            return GetBranches(connectorManager, null, null);
+        }
+
+        private static List<List<ConnectorManager>> GetBranches(this ConnectorManager connectorManager, ConnectorManager previousConnectorManager = null, List<ElementId> elementIds = null)
+        {
+            if (connectorManager == null)
+                return null;
+
+            if (elementIds == null)
+                elementIds = new List<ElementId>();
+
+            List<List<ConnectorManager>> result = new List<List<ConnectorManager>>();
+            int index = elementIds.IndexOf(connectorManager.Owner.Id);
+            if (index != -1)
+            {
+                result.Add(new List<ConnectorManager> { connectorManager });
+                elementIds.RemoveRange(index + 1, elementIds.Count - index - 1);
+                return result;
+            }
+
+            elementIds.Add(connectorManager.Owner.Id);
+
+            List<ConnectorManager> connectorManagerList = GetConnectorManagers(connectorManager);
+            if (connectorManagerList == null || connectorManagerList.Count == 0)
+            {
+                result.Add(new List<ConnectorManager> { connectorManager });
+                return result;
+            }
+
+            if (previousConnectorManager != null)
+                connectorManagerList.RemoveAll(x => x.Owner.Id == previousConnectorManager.Owner.Id);
+
+            if (connectorManagerList == null || connectorManagerList.Count == 0)
+            {
+                result.Add(new List<ConnectorManager> { connectorManager });
+                return result;
+            }
+
+            foreach (ConnectorManager connectorManager_Temp in connectorManagerList)
+            {
+                List<List<ConnectorManager>> connectorManagers = GetBranches(connectorManager_Temp, connectorManager, elementIds);
+                if (connectorManagers == null || connectorManagers.Count == 0)
+                {
+                    result.Add(new List<ConnectorManager> { connectorManager });
+                    continue;
+                }
+                foreach (List<ConnectorManager> aConnectorManagerList_Temp1 in connectorManagers)
+                {
+                    List<ConnectorManager> aConnectorManagerList_Temp2 = new List<ConnectorManager>();
+                    aConnectorManagerList_Temp2.Add(connectorManager);
+
+                    aConnectorManagerList_Temp2.AddRange(aConnectorManagerList_Temp1);
+                    result.Add(aConnectorManagerList_Temp2);
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ SAM_Revit/SAM.Core.Revit/Query/GetConnectorManagers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using Autodesk.Revit.DB;
+
+namespace SAM.Core.Revit
+{
+    public static partial class Query
+    {
+        public static List<ConnectorManager> GetConnectorManagers(this ConnectorManager connectorManager)
+        {
+            if (connectorManager == null)
+                return null;
+
+            Element element = connectorManager.Owner;
+            List<ConnectorManager> result = new List<ConnectorManager>();
+            foreach (Connector connector in connectorManager.Connectors)
+            {
+                foreach (Connector connector_Ref in connector.AllRefs)
+                {
+                    if (connector_Ref.Owner.Id != element.Id && !(connector_Ref.Owner is MEPSystem))
+                        result.Add(connector_Ref.ConnectorManager);
+                }
+            }
+
+            return result;
+        }
+    }
+}

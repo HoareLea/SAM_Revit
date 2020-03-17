24fc3244ae2f24313e41ea4f195dda68f12bc73d
Michal Dengusiak <tassimulation@gmail.com>
2020-03-17 09:14:07 +0000

    Update DefaultSetting.cs

--- SAM_Revit/SAM.Analytical.Revit/Query/DefaultSetting.cs
+++ SAM_Revit/SAM.Analytical.Revit/Query/DefaultSetting.cs
@@ -16,7 +16,23 @@ namespace SAM.Analytical.Revit
             Setting result = new Setting(assembly);
 
             SAMRelationCluster sAMRelationCluster = new SAMRelationCluster();
-            sAMRelationCluster.Add(typeof(Aperture), typeof(FamilyInstance), new SAMRelation("", ""));
+
+            //Aperture
+            //Instance Parameters
+            sAMRelationCluster.Add(typeof(Aperture), typeof(FamilyInstance), new SAMRelation("GetWidth", "SAM_BuildingElementWidth"));
+            sAMRelationCluster.Add(typeof(Aperture), typeof(FamilyInstance), new SAMRelation("GetHeight", "SAM_BuildingElementHeight"));
+            //Type Parameters
+            sAMRelationCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), new SAMRelation(null, "SAM_BuildingElementAir")); //bool
+            sAMRelationCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), new SAMRelation(null, "SAM_BuildingElementColor")); //string or color
+            sAMRelationCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), new SAMRelation(null, "SAM_BuildingElementDesciription")); //string
+            sAMRelationCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), new SAMRelation(null, "SAM_BuildingElementGround")); //bool
+            sAMRelationCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), new SAMRelation(null, "SAM_BuildingElementInternalShadows")); //bool
+            sAMRelationCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), new SAMRelation(null, "SAM_BuildingElementTransparent")); //bool
+
+
+            //sAMRelationCluster.Add(typeof(Aperture), typeof(FamilyInstance), new SAMRelation("GetHeight", "SAM_BuildingElementHeight"));
+            //sAMRelationCluster.Add(typeof(Panel), typeof(Wall), new SAMRelation("GetWidth", "SAM_BuildingElementWidth"));
+            //sAMRelationCluster.Add(typeof(Aperture), typeof(FamilyInstance), new SAMRelation("", ""));
 
             result.Add("Parameter Map", sAMRelationCluster);
             return result;

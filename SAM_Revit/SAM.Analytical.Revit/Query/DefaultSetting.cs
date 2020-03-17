a97db6528629c6edab707c608f8c3c2c123642b4
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-03-17 08:12:10 +0000

    Setting Implementation (WIP)

---
+++ SAM_Revit/SAM.Analytical.Revit/Query/DefaultSetting.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+using Autodesk.Revit.DB;
+
+using SAM.Core;
+
+
+namespace SAM.Analytical.Revit
+{
+    public static partial class Query
+    {
+        public static Setting DefaultSetting()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Setting result = new Setting(assembly);
+
+            SAMRelationCluster sAMRelationCluster = new SAMRelationCluster();
+            sAMRelationCluster.Add(typeof(Aperture), typeof(FamilyInstance), new SAMRelation("", ""));
+
+            result.Add("Parameter Map", sAMRelationCluster);
+            return result;
+        }
+    }
+}

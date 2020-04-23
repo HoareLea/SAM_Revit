6862704de2736282a996d0d0b435b63a0a77404e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-04-23 14:00:09 +0100

    TrimOrExtendWall Component Updated

--- SAM_Revit/SAM.Analytical.Revit/Convert/ToRevit/FamilyInstance.cs
+++ SAM_Revit/SAM.Analytical.Revit/Convert/ToRevit/FamilyInstance.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-using Autodesk.Revit.DB;
+﻿using Autodesk.Revit.DB;
 
 using SAM.Geometry.Revit;
 
--- SAM_Revit/SAM.Analytical.Revit/Modify/TrimOrExtendWall.cs
+++ SAM_Revit/SAM.Analytical.Revit/Modify/TrimOrExtendWall.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Autodesk.Revit.DB;
+
 using SAM.Geometry.Planar;
 using SAM.Geometry.Spatial;
 
@@ -10,7 +11,7 @@ namespace SAM.Analytical.Revit
 {
     public static partial class Modify
     {
-        public static List<Wall> TrimOrExtendWall(this IEnumerable<Wall> walls, double maxDistance)
+        public static List<Wall> TrimOrExtendWall(this IEnumerable<Wall> walls, double maxDistance, double tolerance = Core.Tolerance.Distance)
         {
             Dictionary<double, Dictionary<Wall, Segment2D>> dictionary = new Dictionary<double, Dictionary<Wall, Segment2D>>();
             foreach (Wall wall in walls)
@@ -35,9 +36,10 @@ namespace SAM.Analytical.Revit
             foreach (KeyValuePair<double, Dictionary<Wall, Segment2D>> keyValuePair in dictionary)
             {
                 List<Segment2D> segment2Ds = keyValuePair.Value.Values.ToList();
-                
-                List<Tuple<Wall, Segment2D, List<int>>> tupleList = new List<Tuple<Wall, Segment2D, List<int>>>();
-                foreach(KeyValuePair<Wall, Segment2D> keyValuePair_Wall in keyValuePair.Value)
+
+                //Filtering Walls by Level
+                List<Tuple<Wall, Segment2D, List<int>, bool>> tupleList = new List<Tuple<Wall, Segment2D, List<int>, bool>>();
+                foreach (KeyValuePair<Wall, Segment2D> keyValuePair_Wall in keyValuePair.Value)
                 {
                     LocationCurve locationCurve = keyValuePair_Wall.Key.Location as LocationCurve;
 
@@ -54,66 +56,78 @@ namespace SAM.Analytical.Revit
                         indexes.Add(1);
 
                     if (indexes.Count > 0)
-                        tupleList.Add(new  Tuple<Wall, Segment2D, List<int>>(keyValuePair_Wall.Key, keyValuePair_Wall.Value, indexes));
+                        tupleList.Add(new Tuple<Wall, Segment2D, List<int>, bool>(keyValuePair_Wall.Key, keyValuePair_Wall.Value, indexes, false));
                 }
 
-                
-
-                List<Tuple<Wall, Segment2D>> tupleList_Result = new List<Tuple<Wall, Segment2D>>();
-                foreach(Tuple<Wall, Segment2D, List<int>> tuple in tupleList)
+                //Seeking for walls to be extended/trimmed
+                bool updated = true;
+                while (updated)
                 {
-                    Vector2D direction = tuple.Item2.Direction;
-                    Vector2D direction_Negated = direction.GetNegated();
-
-                    Segment2D segment2D = new Segment2D(tuple.Item2);
-
-                    foreach(int index in tuple.Item3)
+                    updated = false;
+                    for (int i = 0; i < tupleList.Count; i++)
                     {
-                        Vector2D vector2D_1 = Geometry.Planar.Query.TraceFirst(tuple.Item2[index], direction, segment2Ds);
-                        if (vector2D_1 != null && vector2D_1.Length > maxDistance)
-                            vector2D_1 = null;
+                        Tuple<Wall, Segment2D, List<int>, bool> tuple = tupleList[i];
 
-                        Vector2D vector2D_2 = Geometry.Planar.Query.TraceFirst(tuple.Item2[index], direction_Negated, segment2Ds);
-                        if (vector2D_2 != null && vector2D_2.Length > maxDistance)
-                            vector2D_2 = null;
+                        Vector2D direction = tuple.Item2.Direction;
+                        Vector2D direction_Negated = direction.GetNegated();
 
-                        if (vector2D_1 == null && vector2D_2 == null)
+                        Segment2D segment2D = new Segment2D(tuple.Item2);
+
+                        foreach (int index in tuple.Item3)
+                        {
+                            Vector2D vector2D_1 = Geometry.Planar.Query.TraceFirst(tuple.Item2[index], direction, segment2Ds);
+                            if (vector2D_1 != null && vector2D_1.Length > maxDistance)
+                                vector2D_1 = null;
+
+                            Vector2D vector2D_2 = Geometry.Planar.Query.TraceFirst(tuple.Item2[index], direction_Negated, segment2Ds);
+                            if (vector2D_2 != null && vector2D_2.Length > maxDistance)
+                                vector2D_2 = null;
+
+                            if (vector2D_1 == null && vector2D_2 == null)
+                                continue;
+
+                            Vector2D vector2D;
+                            if (vector2D_1 == null)
+                            {
+                                vector2D = vector2D_2;
+                            }
+                            else if (vector2D_2 == null)
+                            {
+                                vector2D = vector2D_1;
+                            }
+                            else
+                            {
+                                if (vector2D_1.Length < vector2D_2.Length)
+                                    vector2D = vector2D_1;
+                                else
+                                    vector2D = vector2D_2;
+                            }
+
+                            if (index == 0)
+                                segment2D = new Segment2D(segment2D[0].GetMoved(vector2D), segment2D[1]);
+                            else
+                                segment2D = new Segment2D(segment2D[0], segment2D[1].GetMoved(vector2D));
+                        }
+
+                        if (segment2D[0] == tuple.Item2[0] && segment2D[1] == tuple.Item2[1])
                             continue;
 
-                        Vector2D vector2D;
-                        if (vector2D_1 == null)
-                        {
-                            vector2D = vector2D_2;
-                        }
-                        else if (vector2D_2 == null)
-                        {
-                            vector2D = vector2D_1;
-                        }
-                        else
-                        {
-                            if (vector2D_1.Length < vector2D_2.Length)
-                                vector2D = vector2D_1;
-                            else
-                                vector2D = vector2D_2;
-                        }
+                        if (segment2D.GetLength() < tolerance)
+                            continue;
 
-                        if (index == 0)
-                            segment2D = new Segment2D(segment2D[0].GetMoved(vector2D), segment2D[1]);
-                        else
-                            segment2D = new Segment2D(segment2D[0], segment2D[1].GetMoved(vector2D));
+                        if (segment2D.AlmostSimilar(tuple.Item2, tolerance))
+                            continue;
+
+                        tupleList[i] = new Tuple<Wall, Segment2D, List<int>, bool>(tuple.Item1, segment2D, tuple.Item3, true);
+                        updated = true;
+                        break;
                     }
-
-                    if (segment2D[0] == tuple.Item2[0] && segment2D[1] == tuple.Item2[1])
-                        continue;
-
-                    if (segment2D.GetLength() < Core.Tolerance.MacroDistance)
-                        continue;
-
-                    tupleList_Result.Add(new Tuple<Wall, Segment2D>(tuple.Item1, segment2D));
                 }
 
-                
-                foreach(Tuple<Wall, Segment2D> tuple in tupleList_Result)
+                tupleList.RemoveAll(x => !x.Item4);
+
+                //Updating Revit Walls
+                foreach (Tuple<Wall, Segment2D, List<int>, bool> tuple in tupleList)
                 {
                     LocationCurve locationCurve = tuple.Item1.Location as LocationCurve;
 
@@ -126,7 +140,8 @@ namespace SAM.Analytical.Revit
                     locationCurve.Curve = line;
 
                     result.Add(tuple.Item1);
-                }                
+                }
+                
             }
 
             return result;

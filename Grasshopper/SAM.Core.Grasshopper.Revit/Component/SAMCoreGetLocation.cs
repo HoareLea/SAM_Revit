4a34126b4749c6bde4ca7f7853811fde69572e8e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-06-10 12:40:28 +0100

    Location Component Added for Grasshopper

---
+++ Grasshopper/SAM.Core.Grasshopper.Revit/Component/SAMCoreGetLocation.cs
@@ -0,0 +1,64 @@
+﻿using Autodesk.Revit.DB;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Core.Grasshopper.Revit.Properties;
+using SAM.Core.Revit;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Core.Grasshopper.Revit
+{
+    public class SAMCoreGetLocation : GH_Component
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("348836ad-6a12-48fc-91ad-f939a8ebda6a");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMCoreGetLocation()
+          : base("SAMCore.GetLocation", "SAMCore.GetLocation",
+              "Gets Location from Revit Document",
+              "SAM", "Core")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            //inputParamManager.AddGenericParameter("_sAMObjects", "_sAMObjects", "SAM Objects", GH_ParamAccess.list);
+            //inputParamManager.AddGenericParameter("_elementIds", "_elementIds", "ElementIds", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooLocationParam(), "Location", "Location", "SAM Location", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+
+            Document document = RhinoInside.Revit.Revit.ActiveDBDocument;
+
+            dataAccess.SetData(0, new GooLocation(document.Location()));
+        }
+    }
+}
---
+++ SAM_Revit/SAM.Core.Revit/Query/Location.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Core.Revit
+{
+    public static partial class Query
+    {
+        public static Location Location(this Document document)
+        {
+            return Location(document?.SiteLocation);
+        }
+
+        public static Location Location(this SiteLocation siteLocation)
+        {
+            if (siteLocation == null)
+                return null;
+
+            return new Location(siteLocation.PlaceName, siteLocation.Longitude, siteLocation.Latitude, siteLocation.Elevation);
+        }
+    }
+}

8da5b0db18248cbb8c3ddc1cefe125b326a82d74
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-08-27 08:27:22 +0100

    Elements Query Added for given BoundingBox3D or Point3D

---
+++ SAM_Revit/SAM.Geometry.Revit/Query/Elements.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.DB;
+using SAM.Geometry.Spatial;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Geometry.Revit
+{
+    public static partial class Query
+    {
+        public static IEnumerable<T> Elements<T>(this Document document, Point3D point3D, double maxDistance = Core.Tolerance.MacroDistance)
+        {
+            if (document == null || point3D == null)
+                return null;
+
+            XYZ xyz = point3D.ToRevit();
+
+            Outline outline = new Outline(new XYZ(xyz.X - maxDistance, xyz.Y - maxDistance, xyz.Z - maxDistance), new XYZ(xyz.X + maxDistance, xyz.Y + maxDistance, xyz.Z + maxDistance));
+
+            LogicalOrFilter logicalOrFilter = new LogicalOrFilter(new ElementFilter[] { new BoundingBoxIntersectsFilter(outline), new BoundingBoxIsInsideFilter(outline) });
+
+            return new FilteredElementCollector(document).OfClass(typeof(T)).WherePasses(logicalOrFilter).Cast<T>();
+        }
+
+        public static IEnumerable<T> Elements<T>(this Document document, BoundingBox3D boundingBox3D)
+        {
+            if (document == null || boundingBox3D == null)
+                return null;
+
+            Outline outline = new Outline(boundingBox3D.Min.ToRevit(), boundingBox3D.Max.ToRevit());
+
+            LogicalOrFilter logicalOrFilter = new LogicalOrFilter(new ElementFilter[] { new BoundingBoxIntersectsFilter(outline), new BoundingBoxIsInsideFilter(outline) });
+
+            return new FilteredElementCollector(document).OfClass(typeof(T)).WherePasses(logicalOrFilter).Cast<T>();
+        }
+    }
+}

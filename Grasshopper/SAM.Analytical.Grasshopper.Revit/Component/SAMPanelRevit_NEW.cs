ebd6e62e907dd58d9de05ed2a9fdfe4fd10331c3
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-08-27 09:56:52 +0100

    Test for Conversion of Analytical Panel to Revit

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Revit/Component/SAMPanelRevit_NEW.cs
@@ -0,0 +1,85 @@
+﻿using Autodesk.Revit.DB;
+using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Revit.Properties;
+using SAM.Core.Grasshopper.Revit;
+using SAM.Core.Revit;
+using System;
+
+namespace SAM.Analytical.Grasshopper.Revit
+{
+    public class SAMPanelRevit_NEW : SAMTransactionComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("c41bed74-4396-4135-be30-96fbf0748fe9");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Revit;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMPanelRevit_NEW()
+          : base("SAMPanel.Revit NEW", "SAMPanel.Revit NEW",
+              "Convert SAM Panel to Revit HostObject",
+              "SAM", "Revit")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new GooPanelParam(), "_panel", "_panel", "SAM Analytical Panel", GH_ParamAccess.item);
+
+            int index = inputParamManager.AddGenericParameter("_convertSettings_", "_convertSettings_", "SAM Convert Settings", GH_ParamAccess.item);
+            inputParamManager[index].Optional = true;
+
+            inputParamManager.AddBooleanParameter("_run_", "_run_", "Run", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new RhinoInside.Revit.GH.Parameters.HostObject(), "hostObject", "hostObject", "Revit HostObject", GH_ParamAccess.item);
+        }
+
+        protected override void TrySolveInstance(IGH_DataAccess dataAccess)
+        {
+            bool run = false;
+            if (!dataAccess.GetData(2, ref run) || !run)
+                return;
+
+            ConvertSettings convertSettings = null;
+            dataAccess.GetData(1, ref convertSettings);
+            convertSettings = this.UpdateSolutionEndEventHandler(convertSettings);
+
+            Panel panel = null;
+            if (!dataAccess.GetData(0, ref panel))
+                return;
+
+            Document document = RhinoInside.Revit.Revit.ActiveDBDocument;
+
+            if (convertSettings.RemoveExisting)
+            {
+                ElementId elementId = panel.ElementId();
+                if (elementId != null && elementId != ElementId.InvalidElementId)
+                {
+                    Element element = document.GetElement(elementId) as SpatialElement;
+                    if (element != null)
+                        document.Delete(elementId);
+                }
+            }
+
+            HostObject hostObject = Analytical.Revit.Convert.ToRevit(panel, document, convertSettings);
+
+            dataAccess.SetData(0, hostObject);
+        }
+    }
+}

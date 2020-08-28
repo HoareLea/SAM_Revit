774d64673a9f05378b62a8a03d06d1d5d6cf130b
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-08-28 14:44:44 +0100

    Create Log Methods Added, New Check Component Added for Grasshopper

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Revit/Component/SAMAnalyticalCheck.cs
@@ -0,0 +1,80 @@
+﻿using Autodesk.Revit.DB;
+using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Revit.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using SAM.Core.Grasshopper.Revit;
+using SAM.Core.Revit;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper.Revit
+{
+    public class SAMAnalyticalCheck : SAMTransactionComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("34a6666d-9e18-4cf3-bc78-11b954675127");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Revit;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalCheck()
+          : base("SAMAnalytical.Check", "SAMAnalytical.Check",
+              "Check Document against analytical object",
+              "SAM", "Revit")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new GooSAMObjectParam<SAMObject>(), "_analytical", "_analytical", "SAM Analytical Object", GH_ParamAccess.item);
+            inputParamManager.AddBooleanParameter("_run_", "_run_", "Run", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooLogParam(), "Log", "Log", "SAM Log", GH_ParamAccess.list);
+        }
+
+        protected override void TrySolveInstance(IGH_DataAccess dataAccess)
+        {
+            bool run = false;
+            if (!dataAccess.GetData(1, ref run) || !run)
+                return;
+
+            SAMObject sAMObject = null;
+            if (!dataAccess.GetData(0, ref sAMObject))
+                return;
+
+            Document document = RhinoInside.Revit.Revit.ActiveDBDocument;
+
+            if (sAMObject is AnalyticalModel)
+                sAMObject = ((AnalyticalModel)sAMObject).AdjacencyCluster;
+            
+            if(!(sAMObject is Panel) && !(sAMObject is Aperture) && !(sAMObject is Space) && !(sAMObject is AdjacencyCluster) && !(sAMObject is AnalyticalModel))
+            {
+                dataAccess.SetData(0, null);
+                return;
+            }
+
+            Log log = Analytical.Revit.Create.Log(sAMObject as dynamic, document);
+
+            dataAccess.SetDataList(0, log);
+        }
+    }
+}
---
+++ SAM_Revit/SAM.Analytical.Revit/Create/Log.cs
@@ -0,0 +1,110 @@
+﻿using Autodesk.Revit.DB;
+using SAM.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Revit
+{
+    public static partial class Create
+    {
+        public static Log Log(this AnalyticalModel analyticalModel, Document document)
+        {
+            if(analyticalModel == null || document == null)
+                return null;
+
+            Log result = new Log();
+            result.AddRange(Log(analyticalModel.AdjacencyCluster, document));
+
+
+            return result;
+        }
+
+        public static Log Log(this AdjacencyCluster adjacencyCluster, Document document)
+        {
+            if (adjacencyCluster == null || document == null)
+                return null;
+
+            Log result = new Log();
+
+            foreach (Construction construction in adjacencyCluster.GetConstructions())
+                result.AddRange(Log(construction, document));
+
+            foreach (ApertureConstruction apertureConstruction in adjacencyCluster.ApertureConstructions())
+                result.AddRange(Log(apertureConstruction, document));
+
+            result.AddRange(Log(adjacencyCluster.GetPanels(), document));
+
+            return result;
+        }
+
+        public static Log Log(this IEnumerable<Panel> panels, Document document)
+        {
+            if (panels == null || document == null)
+                return null;
+
+            Log result = new Log();
+
+            foreach(Panel panel in panels)
+                result.AddRange(Log(panel, document));
+
+            return result;
+        }
+
+        public static Log Log(this Panel panel, Document document)
+        {
+            if (panel == null || document == null)
+                return null;
+
+            Log result = new Log();
+
+            return result;
+        }
+
+        public static Log Log(Construction construction, Document document)
+        {
+            if (construction == null || document == null)
+                return null;
+
+            Log result = new Log();
+
+            return result;
+        }
+
+        public static Log Log(ApertureConstruction apertureConstruction, Document document)
+        {
+            if (apertureConstruction == null || document == null)
+                return null;
+
+            Log result = new Log();
+
+            string name = apertureConstruction.Name;
+
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                result.Add("ApertureConstruction has invalid name Guid: {0}", LogRecordType.Error, apertureConstruction.Guid);
+                return result;
+            }
+
+            BuiltInCategory builtInCategory = apertureConstruction.BuiltInCategory();
+            if(builtInCategory == BuiltInCategory.INVALID)
+            {
+                result.Add("Could not get BuiltInCategory from ApertureConstruction Guid: {0}, Name: {1}", LogRecordType.Error, apertureConstruction.Guid, apertureConstruction.Name);
+                return result;
+            }
+
+            List<FamilySymbol> familySymbols = new FilteredElementCollector(document).OfClass(typeof(FamilySymbol)).OfCategory(builtInCategory).Cast<FamilySymbol>().ToList();
+            if (familySymbols != null && familySymbols.Count != 0)
+            {
+                foreach (FamilySymbol familySymbol in familySymbols)
+                {
+                    string fullName = Core.Revit.Query.FullName(familySymbol);
+                    if (apertureConstruction.Name.Equals(fullName) || apertureConstruction.Name.Equals(familySymbol.Name))
+                        return result;
+                }
+            }
+
+            result.Add("Could not find valid FamilySymbol for ApertureConstruction Guid: {0}, Name: {1}", LogRecordType.Error, apertureConstruction.Guid, apertureConstruction.Name);
+            return result;
+        }
+    }
+}

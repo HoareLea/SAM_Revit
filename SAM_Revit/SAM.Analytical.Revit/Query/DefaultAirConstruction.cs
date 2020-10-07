23b4d859cb9e69151d3d1aede6c1adbc08286ff6
Michal Dengusiak <tassimulation@gmail.com>
2020-10-07 10:29:43 +0100

    Update DefaultAirConstruction.cs

--- SAM_Revit/SAM.Analytical.Revit/Query/DefaultAirConstruction.cs
+++ SAM_Revit/SAM.Analytical.Revit/Query/DefaultAirConstruction.cs
@@ -8,7 +8,7 @@ namespace SAM.Analytical.Revit
         {
             ConstructionLibrary constructionLibrary = new ConstructionLibrary("Test");
 
-            return constructionLibrary?.GetConstructions(panelGroup).FirstOrDefault();
+            return constructionLibrary?.GetConstructions(panelGroup)?.FirstOrDefault();
         }
     }
 }

6702be40ec805db8501f47fc24ff2bf04f3e0283
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-12 14:08:32 +0100

    ElementsByScopeBox Component Added

---
+++ Grasshopper/SAM.Core.Grasshopper.Revit/Component/SAMCoreElementsByScopeBox.cs
@@ -0,0 +1,91 @@
+﻿using Autodesk.Revit.DB;
+using Grasshopper.Kernel;
+using SAM.Core.Grasshopper.Revit.Properties;
+using SAM.Core.Revit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Core.Grasshopper.Revit
+{
+    public class SAMCoreElementsByScopeBox : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("172b87f6-8b7c-444b-be49-dc7b7057a77e");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMCoreElementsByScopeBox()
+          : base("SAMCore.ElementsByScopeBox", "SAMCore.ElementsByScopeBox",
+              "Query Elements By ScopeBox",
+              "SAM", "Core")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            RhinoInside.Revit.GH.Parameters.Element element = new RhinoInside.Revit.GH.Parameters.Element();
+            //element.Optional = true;
+
+            inputParamManager.AddParameter(element, "_scopeBox", "_scopeBox", "Revit ScopeBox", GH_ParamAccess.item);
+            //inputParamManager.AddGenericParameter("_elementIds", "_elementIds", "ElementIds", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new RhinoInside.Revit.GH.Parameters.Element(), "Elements", "Elements", "Revit Elements", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+
+            Document document = RhinoInside.Revit.Revit.ActiveDBDocument;
+
+            Element element = null;
+            if (!dataAccess.GetData(0, ref element) || element == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            if (((BuiltInCategory)element.Category.Id.IntegerValue) != BuiltInCategory.OST_VolumeOfInterest)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            BoundingBoxXYZ boundingBoxXYZ = Core.Revit.Query.BoundingBoxXYZ(element);
+            if(boundingBoxXYZ == null || boundingBoxXYZ.Min.DistanceTo(boundingBoxXYZ.Max) < Tolerance.Distance)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            Outline outline = new Outline(boundingBoxXYZ.Min, boundingBoxXYZ.Max);
+
+            List<Element> elements = new FilteredElementCollector(document).WherePasses(new BoundingBoxIntersectsFilter(outline, Tolerance.MacroDistance)).ToElements()?.ToList();
+
+            dataAccess.SetData(0, elements);
+        }
+    }
+}
---
+++ SAM_Revit/SAM.Core.Revit/Query/BoundingBoxXYZ.cs
@@ -0,0 +1,80 @@
+﻿using Autodesk.Revit.DB;
+using System.Linq;
+
+namespace SAM.Core.Revit
+{
+    public static partial class Query
+    {
+        public static BoundingBoxXYZ BoundingBoxXYZ(this Element scopeBox)
+        {
+            Document document = scopeBox.Document;
+            GeometryElement geometryElement = scopeBox.get_Geometry(document.Application.Create.NewGeometryOptions());
+
+            if (geometryElement ==null || geometryElement.Count() != 12)
+                return null;
+
+            XYZ origin = null;
+            XYZ vx = null;
+            XYZ vy = null;
+            XYZ vz = null;
+
+            foreach (GeometryObject geometryObject in geometryElement)
+            {
+                Line line = geometryObject as Line;
+                if (line == null)
+                    continue;
+
+                XYZ p = line.GetEndPoint(0);
+                XYZ q = line.GetEndPoint(1);
+                XYZ v = q - p;
+
+                if (null == origin)
+                {
+                    origin = p;
+                    vx = v;
+                }
+                else if (p.IsAlmostEqualTo(origin) || q.IsAlmostEqualTo(origin))
+                {
+                    if (q.IsAlmostEqualTo(origin))
+                    {
+                        v = v.Negate();
+                    }
+                    if (vy == null)
+                    {
+                        vy = v;
+                    }
+                    else
+                    {
+                        vz = v;
+
+                        if (0 >= vx.CrossProduct(vy).DotProduct(vz))
+                        {
+                            XYZ tmp = vz;
+                            vz = vy;
+                            vy = tmp;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            // Set up the transform
+
+            Transform t = Transform.Identity;
+            t.Origin = origin;
+            t.BasisX = vx.Normalize();
+            t.BasisY = vy.Normalize();
+            t.BasisZ = vz.Normalize();
+
+
+            // Set up the bounding box
+
+            BoundingBoxXYZ result = new BoundingBoxXYZ();
+            result.Transform = t;
+            result.Min = XYZ.Zero;
+            result.Max = new XYZ(vx.GetLength(), vy.GetLength(), vz.GetLength());
+
+            return result;
+        }
+    }
+}

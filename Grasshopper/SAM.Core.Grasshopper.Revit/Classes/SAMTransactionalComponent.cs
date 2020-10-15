34c0ec3f56c597114337e9c8320600b132285846
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-15 13:05:22 +0100

    Grasshopper Versioning Updated

--- Grasshopper/SAM.Core.Grasshopper.Revit/Classes/SAMTransactionalComponent.cs
+++ Grasshopper/SAM.Core.Grasshopper.Revit/Classes/SAMTransactionalComponent.cs
@@ -1,13 +1,57 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace SAM.Core.Grasshopper.Revit
 {
-    public abstract class SAMTransactionalComponent : RhinoInside.Revit.GH.Components.TransactionalComponent
+    public abstract class SAMTransactionalComponent : RhinoInside.Revit.GH.Components.TransactionalComponent, IGH_SAMComponent
     {
         public SAMTransactionalComponent(string name, string nickname, string description, string category, string subCategory) 
             : base(name, nickname, description, category, subCategory)
         {
-            Message = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            SetValue("SAM_SAMVersion", Core.Query.CurrentVersion());
+            SetValue("SAM_ComponentVersion", LatestComponentVersion);
+
+            Message = LatestComponentVersion;
         }
+
+        public override bool Obsolete
+        {
+
+            get
+            {
+                return Query.Obsolete(this);
+            }
+        }
+
+        public string ComponentVersion
+        {
+            get
+            {
+                return GetValue("SAM_ComponentVersion", null);
+            }
+        }
+
+        public string SAMVersion
+        {
+            get
+            {
+                return GetValue("SAM_SAMVersion", null);
+            }
+        }
+
+        public virtual void AppendAdditionalMenuItems(ToolStripDropDown menu)
+        {
+            base.AppendAdditionalMenuItems(menu);
+            Menu_AppendSeparator(menu);
+            Menu_AppendItem(menu, "Source code", OnSourceCodeClick, Properties.Resources.SAM_Small);
+        }
+
+        public virtual void OnSourceCodeClick(object sender = null, object e = null)
+        {
+            Process.Start("https://github.com/HoareLea/SAM");
+        }
+
+        public abstract string LatestComponentVersion { get; }
     }
 }

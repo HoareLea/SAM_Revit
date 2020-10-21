d5c2f59a8a670ef35b0bf6a61f10a1642b2552b8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-21 10:57:36 +0100

    AppendSourceCodeAdditionalMenuItem Implemented

--- Grasshopper/SAM.Core.Grasshopper.Revit/Classes/SAMTransactionComponent.cs
+++ Grasshopper/SAM.Core.Grasshopper.Revit/Classes/SAMTransactionComponent.cs
@@ -43,14 +43,9 @@ namespace SAM.Core.Grasshopper.Revit
         {
             base.AppendAdditionalMenuItems(menu);
             Menu_AppendSeparator(menu);
-            Menu_AppendItem(menu, "Source code", OnSourceCodeClick, Properties.Resources.SAM_Small);
 
+            this.AppendSourceCodeAdditionalMenuItem(menu);
             this.AppendNewComponentAdditionalMenuItem(menu);
-        }
-
-        public virtual void OnSourceCodeClick(object sender = null, object e = null)
-        {
-            Process.Start("https://github.com/HoareLea/SAM");
         }
 
         public abstract string LatestComponentVersion { get; }

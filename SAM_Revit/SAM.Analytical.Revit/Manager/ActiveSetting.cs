eac9424f8aefa0df5c87a50eeb2fea83b42a029c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-26 10:13:02 +0000

    ActiveSettings Cleaned

--- SAM_Revit/SAM.Analytical.Revit/Manager/ActiveSetting.cs
+++ SAM_Revit/SAM.Analytical.Revit/Manager/ActiveSetting.cs
@@ -10,11 +10,6 @@ namespace SAM.Analytical.Revit
         {
             public const string FileName_DefaultAirConstructionLibrary = "FileName_DefaultAirConstructionLibrary";
             public const string Library_DefaultAirConstructionLibrary = "Library_DefaultAirConstructionLibrary";
-
-            //public const string Construction_Undefined = "Construction_Undefined";
-            //public const string ParameterName_PanelType = "ParameterName_PanelType";
-            //public const string ParameterName_ApertureHeight = "ParameterName_ApertureHeight";
-            //public const string ParameterName_ApertureWidth = "ParameterName_BuildingElementWidth";
         }
 
         private static Setting setting = Load();
@@ -40,35 +35,27 @@ namespace SAM.Analytical.Revit
         {
             Setting result = new Setting(Assembly.GetExecutingAssembly());
 
-            //result.Add(Name.ParameterName_PanelType, "SAM_BuildingElementType");
-            //result.Add(Name.ParameterName_ApertureHeight, "SAM_BuildingElementHeight");
-            //result.Add(Name.ParameterName_ApertureWidth, "SAM_BuildingElementWidth");
-
             MapCluster mapCluster = new MapCluster();
 
             //Aperture
-            //Instance Parameters
             mapCluster.Add(typeof(Aperture), typeof(FamilyInstance), "GetWidth", "SAM_BuildingElementWidth");
             mapCluster.Add(typeof(Aperture), typeof(FamilyInstance), "GetHeight", "SAM_BuildingElementHeight");
-            mapCluster.Add(typeof(Aperture), typeof(FamilyInstance), "Guid", "_Filter Comments 03");  //to be repalced for SAM_GUID
-            mapCluster.Add(typeof(Aperture), typeof(FamilyInstance), null, "_Filter Comments 01");
-            mapCluster.Add(typeof(Aperture), typeof(FamilyInstance), null, "_Filter Comments 02");
-            mapCluster.Add(typeof(Aperture), typeof(FamilyInstance), null, "_Filter Comments 03");
-            //mapCluster.Add(typeof(Aperture), typeof(FamilyInstance), "IsRectangular", "SAM_IsNotValidEditable");  //bool
-            //Type Parameters
-            mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementAir"); //bool
-            mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementColor"); //string or color
-            mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementDesciription"); //string
-            mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementGround"); //bool
-            mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementInternalShadows"); //bool
-            mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementTransparent"); //bool
-            
+            mapCluster.Add(typeof(Aperture), typeof(FamilyInstance), "Guid", "SAM_GUID");
+
+            //ApertureConstruction
+            mapCluster.Add(ApertureConstructionParameter.Color, typeof(FamilySymbol), "SAM_BuildingElementColor");
             mapCluster.Add(ApertureConstructionParameter.DefaultPanelType, typeof(FamilySymbol), "SAM_BuildingElementType");
             mapCluster.Add(ApertureConstructionParameter.Transparent, typeof(FamilySymbol), "SAM_BuildingElementTransparent");
+            mapCluster.Add(ApertureConstructionParameter.Description, typeof(FamilySymbol), "SAM_BuildingElementDescription");
+            //mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementAir"); //bool
+            //mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementGround"); //bool
+            //mapCluster.Add(typeof(ApertureConstruction), typeof(FamilySymbol), null, "SAM_BuildingElementInternalShadows"); //bool
 
+            //Construction
             mapCluster.Add(ConstructionParameter.DefaultPanelType, typeof(FamilySymbol), "SAM_BuildingElementType");
             mapCluster.Add(ConstructionParameter.Description, typeof(FamilySymbol), "SAM_BuildingElementDescription");
 
+            //InternalCondition
             mapCluster.Add(InternalConditionParameter.NumberOfPeople, typeof(Space), "SAM_NoPeople");
             mapCluster.Add(InternalConditionParameter.OccupancyProfileName, typeof(Space), "SAM_OccupacyProfile");
             mapCluster.Add(InternalConditionParameter.OccupantSensibleGain, typeof(Space), "SAM_OccupantSensGain");

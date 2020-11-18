42c7f105fea143d3569a5b4ec45425bbe5445f18
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-11-18 15:08:28 +0000

    Update on Naming for InternalConditionParameters

--- SAM_Revit/SAM.Analytical.Revit/Manager/ActiveSetting.cs
+++ SAM_Revit/SAM.Analytical.Revit/Manager/ActiveSetting.cs
@@ -64,14 +64,14 @@ namespace SAM.Analytical.Revit
             mapCluster.Add(ConstructionParameter.DefaultThickness, typeof(HostObjAttributes), "SAM_BuildingElementThickness");
 
             //InternalCondition
-            mapCluster.Add(InternalConditionParameter.NumberOfPeople, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_NoPeople");
+            mapCluster.Add(InternalConditionParameter.Occupancy, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_NoPeople");
             mapCluster.Add(InternalConditionParameter.OccupancyProfileName, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_OccupacyProfile");
-            mapCluster.Add(InternalConditionParameter.OccupantSensibleGain, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_OccupantSensGain");
-            mapCluster.Add(InternalConditionParameter.OccupantLatentGain, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_OccupantLatGain");
+            mapCluster.Add(InternalConditionParameter.OccupancySensibleGain, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_OccupantSensGain");
+            mapCluster.Add(InternalConditionParameter.OccupancyLatentGain, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_OccupantLatGain");
             mapCluster.Add(InternalConditionParameter.EquipmentSensibleProfileName, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_SmallPowerSensProfile");
             mapCluster.Add(InternalConditionParameter.EquipmentLatentProfileName, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_SmallPowerLatProfile");
             mapCluster.Add(InternalConditionParameter.LightingGain, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_GenLightingGain");
-            mapCluster.Add(InternalConditionParameter.DesignLuxLevel, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_DesignLuxLevel");
+            mapCluster.Add(InternalConditionParameter.LightingLevel, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_DesignLuxLevel");
 
             //Space
             mapCluster.Add(SpaceParameter.Area, typeof(Autodesk.Revit.DB.Mechanical.Space), "SAM_Area");

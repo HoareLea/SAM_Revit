0a5cadd31a04912387d49a6c85e8eefe8ef91f0c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-05 08:53:46 +0000

    Formulas Added for Some of the Parameters

---
+++ SAM_Revit/SAM.Analytical.Revit/Query/OccupiedHours25Percentage.cs
@@ -0,0 +1,22 @@
+﻿namespace SAM.Analytical.Revit
+{
+    public static partial class Query
+    {
+        public static double OccupiedHours25Percentage(SpaceSimulationResult spaceSimulationResult)
+        {
+            if (spaceSimulationResult == null)
+                return double.NaN;
+
+            if (!spaceSimulationResult.TryGetValue(SpaceSimulationResultParameter.OccupiedHours25, out int occupiedHours25))
+                return double.NaN;
+
+            if (!spaceSimulationResult.TryGetValue(SpaceSimulationResultParameter.OccupiedHours, out int occupiedHours))
+                return double.NaN;
+
+            if (occupiedHours == 0)
+                return 0;
+
+            return (occupiedHours / occupiedHours25) * 100;
+        }
+    }
+}
---
+++ SAM_Revit/SAM.Analytical.Revit/Query/OccupiedHours28Percentage.cs
@@ -0,0 +1,22 @@
+﻿namespace SAM.Analytical.Revit
+{
+    public static partial class Query
+    {
+        public static double OccupiedHours28Percentage(SpaceSimulationResult spaceSimulationResult)
+        {
+            if (spaceSimulationResult == null)
+                return double.NaN;
+
+            if (!spaceSimulationResult.TryGetValue(SpaceSimulationResultParameter.OccupiedHours28, out int occupiedHours28))
+                return double.NaN;
+
+            if (!spaceSimulationResult.TryGetValue(SpaceSimulationResultParameter.OccupiedHours, out int occupiedHours))
+                return double.NaN;
+
+            if (occupiedHours == 0)
+                return 0;
+
+            return (occupiedHours / occupiedHours28) * 100;
+        }
+    }
+}

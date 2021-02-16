9c25ae34e24b8a2949b08ee6ce9ff704d45116b4
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-16 12:36:55 +0000

    CreateSheets Grasshopper Component Added

---
+++ Grasshopper/SAM.Core.Grasshopper.Revit/Component/SAMCoreCreateSheets.cs
@@ -0,0 +1,87 @@
+﻿using Autodesk.Revit.DB;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Core;
+using SAM.Core.Grasshopper.Revit;
+using SAM.Core.Grasshopper.Revit.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper.Revit
+{
+    public class SAMCoreCreateSheets : SAMTransactionComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("65f51465-a92c-4c08-8308-01f0c542447d");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Revit;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMCoreCreateSheets()
+          : base("SAMCore.CreateSheets", "SAMCore.CreateSheets",
+              "Create Sheets",
+              "SAM", "Revit")
+        {
+
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new RhinoInside.Revit.GH.Parameters.View(), "_referenceViewSheet", "_referenceViewSheet", "Elements to be deleted", GH_ParamAccess.item);
+            inputParamManager.AddTextParameter("_templateNames", "_templateNames", "View Templates Names", GH_ParamAccess.list);
+
+            inputParamManager.AddBooleanParameter("_run", "_run", "Run", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new RhinoInside.Revit.GH.Parameters.View(), "ViewSheets", "ViewSheets", "ViewSheets", GH_ParamAccess.list);
+        }
+
+        protected override void TrySolveInstance(IGH_DataAccess dataAccess)
+        {
+            bool run = false;
+            if (!dataAccess.GetData(2, ref run) || !run)
+                return;
+
+
+            View view = null;
+            if (!dataAccess.GetData(0, ref view) || !(view is ViewSheet))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+
+            List<string> templateNames = new List<string>();
+            if (!dataAccess.GetDataList(1, templateNames))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<ViewSheet> viewSheets = Core.Revit.Modify.CreateSheets((ViewSheet)view, templateNames);
+
+            dataAccess.SetDataList(0, viewSheets);
+        }
+    }
+}
---
+++ SAM_Revit/SAM.Core.Revit/Modify/CreateSheets.cs
@@ -0,0 +1,90 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Core.Revit
+{
+    public static partial class Modify
+    {
+        public static List<ViewSheet> CreateSheets(this ViewSheet referenceViewSheet, IEnumerable<string> templateNames)
+        {
+            if (referenceViewSheet == null || templateNames == null || templateNames.Count() == 0)
+                return null;
+
+            Document document = referenceViewSheet.Document;
+
+            FamilyInstance familyInstance_TitleBlock = new FilteredElementCollector(document, referenceViewSheet.Id).OfCategory(BuiltInCategory.OST_TitleBlocks).OfClass(typeof(FamilyInstance)).Cast<FamilyInstance>().FirstOrDefault();
+            if (familyInstance_TitleBlock == null)
+                return null;
+
+            Viewport viewport = new FilteredElementCollector(document, referenceViewSheet.Id).OfClass(typeof(Viewport)).Cast<Viewport>().FirstOrDefault();
+            if (viewport == null)
+                return null;
+
+            XYZ xyz = viewport.GetBoxCenter();
+            if (xyz == null)
+                return null;
+
+            ElementId elementId_TitleBlockType = familyInstance_TitleBlock.GetTypeId();
+            if (elementId_TitleBlockType == null || elementId_TitleBlockType == ElementId.InvalidElementId)
+                return null;
+
+            IEnumerable<View> views_All = new FilteredElementCollector(document).OfClass(typeof(View)).Cast<View>();
+            if (views_All == null || views_All.Count() == 0)
+                return null;
+
+            List<View> views_Templates = new List<View>();
+            List<View> views = new List<View>();
+            foreach (View view in views_All)
+            {
+                if (referenceViewSheet.ViewType != view.ViewType || referenceViewSheet.Id == view.Id)
+                    continue;
+
+                if (view.IsTemplate)
+                    views_Templates.Add(view);
+                else
+                    views.Add(view);
+            }
+
+            List<ElementId> elementIds = new List<ElementId>();
+            foreach(View view_Template in views_Templates)
+            {
+                if (!templateNames.Contains(view_Template.Name))
+                    continue;
+
+                elementIds.Add(view_Template.Id);
+            }
+
+            if (elementIds == null || elementIds.Count == 0)
+                return null;
+
+            List<ViewSheet> result = new List<ViewSheet>();
+            foreach(View view in views)
+            {
+                if (!elementIds.Contains(view.ViewTemplateId))
+                    continue;
+
+                View viewTemplate = document.GetElement(view.ViewTemplateId) as View;
+
+                ViewSheet viewSheet_New = ViewSheet.Create(document, elementId_TitleBlockType);
+                if (viewSheet_New == null)
+                    continue;
+
+                try
+                {
+                    viewSheet_New.Name = string.Format("{0}_{1}", viewTemplate.Name, view.GenLevel.Name);
+                }
+                catch(System.Exception exception)
+                {
+
+                }
+
+                Viewport viewport_New = Viewport.Create(document, viewSheet_New.Id, view.Id, xyz);
+
+                result.Add(viewSheet_New);
+            }
+
+            return result;
+        }
+    }
+}

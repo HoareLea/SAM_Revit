81b0ae4c2e9531546bdc2e06ce7cf544733b8057
Michal Dengusiak <tassimulation@gmail.com>
2021-03-05 12:39:43 +0000

    Update Segment3Ds.cs

--- SAM_Revit/SAM.Geometry.Revit/Convert/ToSAM/Segment3Ds.cs
+++ SAM_Revit/SAM.Geometry.Revit/Convert/ToSAM/Segment3Ds.cs
@@ -15,19 +15,25 @@ namespace SAM.Geometry.Revit
             if (curve is Line)
                 return new List<Segment3D>() { ToSAM((Line)curve) };
 
-            List<Point3D> point3Ds = curve.Tessellate()?.ToList().ConvertAll(x => x.ToSAM());
-            if (point3Ds == null || point3Ds.Count() == 0)
-                return new List<Segment3D>() { ToSAM_Segment3D(curve) };
+            ISegmentable3D segmentable3D = ToSAM(curve) as ISegmentable3D;
+            if (segmentable3D == null)
+                return null;
 
-            List<Segment3D> result = new List<Segment3D>();
-
-            for (int i = 0; i < point3Ds.Count - 1; i++)
-                result.Add(new Segment3D(point3Ds[i], point3Ds[i + 1]));
-
-            if (!curve.IsBound && point3Ds.Count > 2)
-                result.Add(new Segment3D(point3Ds.Last(), point3Ds.First()));
+            List<Segment3D> result = segmentable3D.GetSegments();
+            if(!curve.IsBound && result.Count > 1 && result[0][0] != result[result.Count - 1][1])
+                result.Add(new Segment3D(result[result.Count - 1][1], result[0][0]));
 
             return result;
+
+            //List<Point3D> point3Ds = curve.Tessellate()?.ToList().ConvertAll(x => x.ToSAM());
+            //if (point3Ds == null || point3Ds.Count() == 0)
+            //    return new List<Segment3D>() { ToSAM_Segment3D(curve) };
+
+            //for (int i = 0; i < point3Ds.Count - 1; i++)
+            //    result.Add(new Segment3D(point3Ds[i], point3Ds[i + 1]));
+
+            //if (!curve.IsBound && point3Ds.Count > 2)
+            //    result.Add(new Segment3D(point3Ds.Last(), point3Ds.First()));
         }
     }
 }

9787e801b66a5c271664705fd427b6b127e03ee2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-03-10 15:44:05 +0000

    DuplicatePlanView Grasshopper Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Revit/Component/RevitDuplicatePlanView.cs
@@ -0,0 +1,85 @@
+﻿using Autodesk.Revit.DB;
+using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Revit.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper.Revit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper.Revit
+{
+    public class RevitDuplicatePlanView : SAMTransactionComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("2e88344e-24d3-4300-828b-8d128d5d25ec");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.1";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Revit;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public RevitDuplicatePlanView()
+          : base("Revit.DuplicatePlanView", "Revit.DuplicatePlanView",
+              "Duplicate PlanView by given ViewPlan in Levels",
+              "SAM", "Revit")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new RhinoInside.Revit.GH.Parameters.View(), "_viewPlan", "_viewPlan", "Revit ViewPlan", GH_ParamAccess.item);
+
+            RhinoInside.Revit.GH.Parameters.Level level = new RhinoInside.Revit.GH.Parameters.Level() { Name = "levels_", NickName = "levels_", Description = "Revit Levels", Access = GH_ParamAccess.list, Optional = true };
+            inputParamManager.AddParameter(level);
+
+            inputParamManager.AddBooleanParameter("_run", "_run", "Run", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new RhinoInside.Revit.GH.Parameters.View(), "ViewPlans", "ViewPlans", "Revit ViewPlans", GH_ParamAccess.list);
+        }
+
+        protected override void TrySolveInstance(IGH_DataAccess dataAccess)
+        {
+            bool run = false;
+            if (!dataAccess.GetData(2, ref run) || !run)
+                return;
+
+            ViewPlan viewPlan = null;
+            if (!dataAccess.GetData(1, ref viewPlan))
+                return;
+
+            if(viewPlan == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<Level> levels = new List<Level>();
+            if (!dataAccess.GetDataList(2, levels))
+                levels = null;
+
+            List<ViewPlan> result = Core.Revit.Modify.DuplicateViewPlan(viewPlan, levels);
+
+            dataAccess.SetData(0, result);
+        }
+    }
+}
---
+++ SAM_Revit/SAM.Core.Revit/Modify/DuplicateViewPlan.cs
@@ -0,0 +1,86 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Core.Revit
+{
+    public static partial class Modify
+    {
+        public static List<ViewPlan> DuplicateViewPlan(this ViewPlan viewPlan, IEnumerable<Level> levels = null)
+        {
+            if (viewPlan == null || !viewPlan.IsValidObject || viewPlan.IsTemplate)
+                return null;
+
+            Document document = viewPlan.Document;
+            if (document == null)
+                return null;
+
+            List<Level> levels_Temp = null;
+            if(levels != null)
+                levels_Temp = new List<Level>(levels);
+
+            if (levels_Temp == null)
+                levels_Temp = new FilteredElementCollector(document).OfClass(typeof(Level)).Cast<Level>().ToList();
+
+
+            List<ViewPlan> result = new List<ViewPlan>();
+            foreach(Level level in levels_Temp)
+            {
+                if (level.Id == viewPlan.GenLevel.Id)
+                    continue;
+                
+                ViewPlan viewPlan_New = ViewPlan.Create(document, viewPlan.GetTypeId(), level.Id);
+                foreach(Parameter parameter in viewPlan.ParametersMap)
+                {
+                    Definition definition = parameter?.Definition;
+                    if (definition == null)
+                        continue;
+
+                    Parameter parameter_New = viewPlan_New.get_Parameter(parameter.Definition);
+                    if (parameter_New == null)
+                        continue;
+
+                    CopyValue(parameter, parameter_New);
+                }
+
+                result.Add(viewPlan_New);
+
+                IEnumerable<ElementId> elementIds_Dependent = viewPlan.GetDependentViewIds();
+                if(elementIds_Dependent != null && elementIds_Dependent.Count() != 0)
+                {
+                    foreach(ElementId elementId_Dependent in elementIds_Dependent)
+                    {
+                        ViewPlan viewPlan_Dependent = document.GetElement(elementId_Dependent) as ViewPlan;
+                        if (viewPlan_Dependent == null)
+                            continue;
+
+                        ElementId elementId_Dependent_New = viewPlan_New.Duplicate(ViewDuplicateOption.AsDependent);
+                        if (elementId_Dependent_New == null || elementId_Dependent_New == ElementId.InvalidElementId)
+                            continue;
+
+                        ViewPlan viewPlan_Dependent_New = document.GetElement(elementId_Dependent_New) as ViewPlan;
+                        if (viewPlan_Dependent_New == null)
+                            continue;
+
+                        foreach (Parameter parameter in viewPlan_Dependent.ParametersMap)
+                        {
+                            Definition definition = parameter?.Definition;
+                            if (definition == null)
+                                continue;
+
+                            Parameter parameter_New = viewPlan_Dependent_New.get_Parameter(parameter.Definition);
+                            if (parameter_New == null)
+                                continue;
+
+                            CopyValue(parameter, parameter_New);
+                        }
+
+                        result.Add(viewPlan_Dependent_New);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}

4fd307c1af73d25d661dbb90f19138f27de8984e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-04-08 23:27:44 +0100

    Create Shells from Spaces Optimisation

--- SAM_Revit/SAM.Analytical.Revit/Create/Shells.cs
+++ SAM_Revit/SAM.Analytical.Revit/Create/Shells.cs
@@ -8,11 +8,143 @@ namespace SAM.Analytical.Revit
 {
     public static partial class Create
     {
+        //public static List<Shell> Shells_Obsolete(this Document document, IEnumerable<Autodesk.Revit.DB.Mechanical.Space> spaces = null, double offset = 0.1, double snapTolerance = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.Distance)
+        //{
+        //    if (document == null)
+        //        return null;
+
+        //    List<Autodesk.Revit.DB.Mechanical.Space> spaces_Temp = new FilteredElementCollector(document).OfCategory(BuiltInCategory.OST_MEPSpaces).Cast<Autodesk.Revit.DB.Mechanical.Space>().ToList();
+        //    if (spaces != null)
+        //    {
+        //        List<Autodesk.Revit.DB.Mechanical.Space> spaces_New = new List<Autodesk.Revit.DB.Mechanical.Space>();
+        //        foreach (Autodesk.Revit.DB.Mechanical.Space space in spaces)
+        //        {
+        //            int index = spaces_Temp.FindIndex(x => x.Id.IntegerValue == space.Id.IntegerValue);
+        //            if (index != -1)
+        //                spaces_New.Add(spaces_Temp[index]);
+        //        }
+        //        spaces_Temp = spaces_New;
+        //    }
+
+        //    if (spaces_Temp == null || spaces_Temp.Count == 0)
+        //        return null;
+
+        //    List<Wall> walls = new FilteredElementCollector(document).OfClass(typeof(Wall)).Cast<Wall>().ToList();
+        //    if (walls == null || walls.Count == 0)
+        //        return null;
+
+        //    List<Panel> panels = new List<Panel>();
+        //    foreach (Wall wall in walls)
+        //    {
+        //        List<Panel> panels_Wall = wall.ToSAM(new Core.Revit.ConvertSettings(true, false, false));
+        //        if (panels_Wall == null || panels_Wall.Count == 0)
+        //            continue;
+
+        //        panels.AddRange(panels_Wall);
+        //    }
+
+        //    List<Shell> shells = new List<Shell>();
+        //    foreach (Autodesk.Revit.DB.Mechanical.Space space in spaces_Temp)
+        //    {
+        //        XYZ xyz = (space.Location as LocationPoint)?.Point;
+        //        if (xyz == null)
+        //            continue;
+
+        //        double elevation_Top = double.NaN;
+
+        //        Parameter parameter = space.get_Parameter(BuiltInParameter.ROOM_UPPER_LEVEL);
+        //        if (parameter != null && parameter.HasValue)
+        //        {
+        //            ElementId elementId = parameter.AsElementId();
+        //            if (elementId != null && elementId != ElementId.InvalidElementId)
+        //            {
+        //                Level level = document.GetElement(elementId) as Level;
+        //                if (level != null)
+        //                    elevation_Top = UnitUtils.ConvertFromInternalUnits(level.Elevation, DisplayUnitType.DUT_METERS);
+        //            }
+        //        }
+
+        //        BoundingBoxXYZ boundingBoxXYZ = space.get_BoundingBox(null);
+        //        if (boundingBoxXYZ == null || boundingBoxXYZ.Min.Z == boundingBoxXYZ.Max.Z)
+        //            continue;
+
+        //        if (double.IsNaN(elevation_Top))
+        //            elevation_Top = UnitUtils.ConvertFromInternalUnits(boundingBoxXYZ.Max.Z, DisplayUnitType.DUT_METERS);
+
+        //        double elevation_Bottom = UnitUtils.ConvertFromInternalUnits(boundingBoxXYZ.Min.Z, DisplayUnitType.DUT_METERS);
+
+        //        Geometry.Spatial.Plane plane = Geometry.Spatial.Plane.WorldXY.GetMoved(new Vector3D(0, 0, elevation_Bottom + offset)) as Geometry.Spatial.Plane;
+
+        //        Point3D point3D = Geometry.Revit.Convert.ToSAM(xyz);
+        //        if (point3D == null)
+        //            continue;
+
+        //        Geometry.Planar.Point2D point2D = plane.Convert(point3D);
+
+        //        List<Geometry.Planar.Segment2D> segment2Ds = new List<Geometry.Planar.Segment2D>();
+        //        foreach (Panel panel in panels)
+        //        {
+        //            Face3D face3D = panel.GetFace3D();
+        //            if (face3D == null)
+        //                continue;
+
+        //            face3D = new Face3D(face3D.GetExternalEdge3D());
+
+        //            PlanarIntersectionResult planarIntersectionResult = plane.Intersection(face3D);
+        //            if (planarIntersectionResult == null)
+        //                continue;
+
+        //            List<Geometry.Planar.ISegmentable2D> segmentable2Ds_Temp = planarIntersectionResult.GetGeometry2Ds<Geometry.Planar.ISegmentable2D>();
+        //            if (segmentable2Ds_Temp == null || segmentable2Ds_Temp.Count == 0)
+        //                continue;
+
+        //            foreach (Geometry.Planar.ISegmentable2D segmentable2D in segmentable2Ds_Temp)
+        //                segment2Ds.AddRange(segmentable2D.GetSegments());
+        //        }
+
+        //        if (segment2Ds == null || segment2Ds.Count == 0)
+        //            continue;
+
+        //        segment2Ds = Geometry.Planar.Query.Split(segment2Ds, tolerance);
+
+        //        segment2Ds = Geometry.Planar.Query.Snap(segment2Ds, true, snapTolerance);
+
+        //        List<Geometry.Planar.Polygon2D> polygon2Ds = Geometry.Planar.Create.Polygon2Ds(segment2Ds, tolerance);
+        //        if (polygon2Ds == null || polygon2Ds.Count == 0)
+        //            continue;
+
+        //        polygon2Ds = polygon2Ds.FindAll(x => x.Inside(point2D));
+        //        if (polygon2Ds == null || polygon2Ds.Count == 0)
+        //            continue;
+
+        //        List<Geometry.Planar.Face2D> face2Ds = Geometry.Planar.Create.Face2Ds(polygon2Ds, true);
+        //        if (face2Ds == null || face2Ds.Count == 0)
+        //            continue;
+
+        //        Geometry.Spatial.Plane plane_Bottom = Geometry.Spatial.Plane.WorldXY.GetMoved(new Vector3D(0, 0, elevation_Bottom)) as Geometry.Spatial.Plane;
+        //        Geometry.Spatial.Plane plane_Top = Geometry.Spatial.Plane.WorldXY.GetMoved(new Vector3D(0, 0, elevation_Top)) as Geometry.Spatial.Plane;
+
+        //        List<Face3D> face3Ds = new List<Face3D>();
+        //        face3Ds.AddRange(face2Ds.ConvertAll(x => new Face3D(plane_Bottom, x)));
+        //        face3Ds.AddRange(face2Ds.ConvertAll(x => new Face3D(plane_Top, x)));
+
+        //        List<Shell> shells_Temp = Geometry.Spatial.Create.Shells(face3Ds, tolerance);
+        //        if (shells_Temp == null || shells_Temp.Count == 0)
+        //            continue;
+
+        //        shells.AddRange(shells_Temp);
+
+        //    }
+
+        //    return shells;
+        //}
+
         public static List<Shell> Shells(this Document document, IEnumerable<Autodesk.Revit.DB.Mechanical.Space> spaces = null, double offset = 0.1, double snapTolerance = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.Distance)
         {
             if (document == null)
                 return null;
 
+            //Collecting Space list
             List<Autodesk.Revit.DB.Mechanical.Space> spaces_Temp = new FilteredElementCollector(document).OfCategory(BuiltInCategory.OST_MEPSpaces).Cast<Autodesk.Revit.DB.Mechanical.Space>().ToList();
             if(spaces != null)
             {
@@ -29,21 +161,13 @@ namespace SAM.Analytical.Revit
             if (spaces_Temp == null || spaces_Temp.Count == 0)
                 return null;
 
-            List<Wall> walls = new FilteredElementCollector(document).OfClass(typeof(Wall)).Cast<Wall>().ToList();
-            if (walls == null || walls.Count == 0)
-                return null;
+            //Dictionary of bottom elevations and tuples (top elevation, location 2D, Space) (Metric Units)
+            Dictionary<double, List<Tuple<double, Geometry.Planar.Point2D, Autodesk.Revit.DB.Mechanical.Space>>> dictionary = new Dictionary<double, List<Tuple<double, Geometry.Planar.Point2D, Autodesk.Revit.DB.Mechanical.Space>>>();
+            
+            //Space cut elevations (Imperial Units)
+            HashSet<double> cutElevations = new HashSet<double>();
 
-            List<Panel> panels = new List<Panel>();
-            foreach(Wall wall in walls)
-            {
-                List<Panel> panels_Wall = wall.ToSAM(new Core.Revit.ConvertSettings(true, false, false));
-                if (panels_Wall == null || panels_Wall.Count == 0)
-                    continue;
-
-                panels.AddRange(panels_Wall);
-            }
-
-            List<Shell> shells = new List<Shell>();
+            //Collecting Spaces data
             foreach (Autodesk.Revit.DB.Mechanical.Space space in spaces_Temp)
             {
                 XYZ xyz = (space.Location as LocationPoint)?.Point;
@@ -53,13 +177,13 @@ namespace SAM.Analytical.Revit
                 double elevation_Top = double.NaN;
 
                 Parameter parameter = space.get_Parameter(BuiltInParameter.ROOM_UPPER_LEVEL);
-                if(parameter != null && parameter.HasValue)
+                if (parameter != null && parameter.HasValue)
                 {
                     ElementId elementId = parameter.AsElementId();
-                    if(elementId != null && elementId != ElementId.InvalidElementId)
+                    if (elementId != null && elementId != ElementId.InvalidElementId)
                     {
                         Level level = document.GetElement(elementId) as Level;
-                        if(level != null)
+                        if (level != null)
                             elevation_Top = UnitUtils.ConvertFromInternalUnits(level.Elevation, DisplayUnitType.DUT_METERS);
                     }
                 }
@@ -73,70 +197,135 @@ namespace SAM.Analytical.Revit
 
                 double elevation_Bottom = UnitUtils.ConvertFromInternalUnits(boundingBoxXYZ.Min.Z, DisplayUnitType.DUT_METERS);
 
-                Geometry.Spatial.Plane plane = Geometry.Spatial.Plane.WorldXY.GetMoved(new Vector3D(0, 0, elevation_Bottom + offset)) as Geometry.Spatial.Plane;
-
                 Point3D point3D = Geometry.Revit.Convert.ToSAM(xyz);
                 if (point3D == null)
                     continue;
 
-                Geometry.Planar.Point2D point2D = plane.Convert(point3D);
+                Geometry.Planar.Point2D point2D = Geometry.Spatial.Plane.WorldXY.Convert(point3D);
+                if (point2D == null)
+                    continue;
+
+                cutElevations.Add(boundingBoxXYZ.Min.Z + UnitUtils.ConvertToInternalUnits(offset, DisplayUnitType.DUT_METERS));
+                if(!dictionary.TryGetValue(elevation_Bottom, out List<Tuple<double, Geometry.Planar.Point2D, Autodesk.Revit.DB.Mechanical.Space>> tuples))
+                {
+                    tuples = new List<Tuple<double, Geometry.Planar.Point2D, Autodesk.Revit.DB.Mechanical.Space>>();
+                    dictionary[elevation_Bottom] = tuples;
+                }
+                tuples.Add(new Tuple<double, Geometry.Planar.Point2D, Autodesk.Revit.DB.Mechanical.Space>(elevation_Top, point2D, space));
+            }
+
+            //Collecting Revit Walls
+            List<Wall> walls = new FilteredElementCollector(document).OfClass(typeof(Wall)).Cast<Wall>().ToList();
+            if (walls == null || walls.Count == 0)
+                return null;
+
+            //Converting Revit Walls to SAM Panels
+            List<Panel> panels = new List<Panel>();
+            foreach(Wall wall in walls)
+            {
+                BoundingBoxXYZ boundingBoxXYZ = wall?.get_BoundingBox(null);
+                if (boundingBoxXYZ == null)
+                {
+                    continue;
+                }
+
+                bool valid = false;
+                foreach(double cutElevation in cutElevations)
+                {
+                    if(boundingBoxXYZ.Max.Z >= cutElevation && boundingBoxXYZ.Min.Z <= cutElevation)
+                    {
+                        valid = true;
+                        break;
+                    }
+                }
+
+                if(!valid)
+                {
+                    continue;
+                }
+                
+                List<Panel> panels_Wall = wall.ToSAM(new Core.Revit.ConvertSettings(true, false, false));
+                if (panels_Wall == null || panels_Wall.Count == 0)
+                    continue;
+
+                panels.AddRange(panels_Wall);
+            }
+
+            if (panels == null || panels.Count == 0)
+                return null;
+
+            List<Shell> result = new List<Shell>();
+
+            //Inerating through elevations and Spaces data
+            foreach(KeyValuePair<double, List<Tuple<double, Geometry.Planar.Point2D, Autodesk.Revit.DB.Mechanical.Space>>> keyValuePair in dictionary)
+            {
+                double elevation_Bottom = keyValuePair.Key;
+                double elevation_Cut = elevation_Bottom + offset;
+
+                Geometry.Spatial.Plane plane_Cut = Geometry.Spatial.Plane.WorldXY.GetMoved(new Vector3D(0, 0, elevation_Cut)) as Geometry.Spatial.Plane;
 
                 List<Geometry.Planar.Segment2D> segment2Ds = new List<Geometry.Planar.Segment2D>();
                 foreach (Panel panel in panels)
                 {
-                    Face3D face3D = panel.GetFace3D();
-                    if (face3D == null)
+                    IClosedPlanar3D closedPlanar3D = panel?.GetFace3D()?.GetExternalEdge3D();
+                    if (closedPlanar3D == null)
                         continue;
 
-                    face3D = new Face3D(face3D.GetExternalEdge3D());
-
-                    PlanarIntersectionResult planarIntersectionResult = plane.Intersection(face3D);
-                    if (planarIntersectionResult == null)
+                    PlanarIntersectionResult planarIntersectionResult = plane_Cut.Intersection(closedPlanar3D);
+                    if (planarIntersectionResult == null || !planarIntersectionResult.Intersecting)
+                    {
                         continue;
+                    }
 
                     List<Geometry.Planar.ISegmentable2D> segmentable2Ds_Temp = planarIntersectionResult.GetGeometry2Ds<Geometry.Planar.ISegmentable2D>();
                     if (segmentable2Ds_Temp == null || segmentable2Ds_Temp.Count == 0)
+                    {
                         continue;
+                    }
 
-                    foreach (Geometry.Planar.ISegmentable2D segmentable2D in segmentable2Ds_Temp)
-                        segment2Ds.AddRange(segmentable2D.GetSegments());
+                    segmentable2Ds_Temp?.ForEach(x => segment2Ds.AddRange(x.GetSegments()));
                 }
 
-                if (segment2Ds == null || segment2Ds.Count == 0)
+                if (panels == null || panels.Count == 0)
                     continue;
 
                 segment2Ds = Geometry.Planar.Query.Split(segment2Ds, tolerance);
 
                 segment2Ds = Geometry.Planar.Query.Snap(segment2Ds, true, snapTolerance);
 
-                List<Geometry.Planar.Polygon2D> polygon2Ds = Geometry.Planar.Create.Polygon2Ds(segment2Ds, tolerance);
-                if (polygon2Ds == null || polygon2Ds.Count == 0)
-                    continue;
-
-                polygon2Ds = polygon2Ds.FindAll(x => x.Inside(point2D));
-                if (polygon2Ds == null || polygon2Ds.Count == 0)
-                    continue;
-
-                List<Geometry.Planar.Face2D> face2Ds = Geometry.Planar.Create.Face2Ds(polygon2Ds, true);
-                if (face2Ds == null || face2Ds.Count == 0)
+                List<Tuple<Geometry.Planar.BoundingBox2D, Geometry.Planar.Polygon2D>> tuples_Polygon2D = Geometry.Planar.Create.Polygon2Ds(segment2Ds, tolerance)?.ConvertAll(x => new Tuple<Geometry.Planar.BoundingBox2D, Geometry.Planar.Polygon2D>(x.GetBoundingBox(tolerance), x));
+                if (tuples_Polygon2D == null || tuples_Polygon2D.Count == 0)
                     continue;
 
                 Geometry.Spatial.Plane plane_Bottom = Geometry.Spatial.Plane.WorldXY.GetMoved(new Vector3D(0, 0, elevation_Bottom)) as Geometry.Spatial.Plane;
-                Geometry.Spatial.Plane plane_Top = Geometry.Spatial.Plane.WorldXY.GetMoved(new Vector3D(0, 0, elevation_Top)) as Geometry.Spatial.Plane;
 
-                List<Face3D> face3Ds = new List<Face3D>();
-                face3Ds.AddRange(face2Ds.ConvertAll(x => new Face3D(plane_Bottom, x)));
-                face3Ds.AddRange(face2Ds.ConvertAll(x => new Face3D(plane_Top, x)));
+                foreach (Tuple<double, Geometry.Planar.Point2D, Autodesk.Revit.DB.Mechanical.Space> tuple in keyValuePair.Value)
+                {
+                    Geometry.Spatial.Plane plane_Top = Geometry.Spatial.Plane.WorldXY.GetMoved(new Vector3D(0, 0, tuple.Item1)) as Geometry.Spatial.Plane;
 
-                List<Shell> shells_Temp = Geometry.Spatial.Create.Shells(face3Ds, tolerance);
-                if (shells_Temp == null || shells_Temp.Count == 0)
-                    continue;
+                    Geometry.Planar.Point2D point2D = tuple.Item2;
 
-                shells.AddRange(shells_Temp);
+                    List<Geometry.Planar.Polygon2D> polygon2Ds = tuples_Polygon2D.FindAll(x => x.Item1.Inside(point2D, tolerance)).FindAll(x => x.Item2.Inside(point2D, tolerance)).ConvertAll(x => x.Item2);
+                    if (polygon2Ds == null || polygon2Ds.Count == 0)
+                        continue;
 
+                    List<Geometry.Planar.Face2D> face2Ds = Geometry.Planar.Create.Face2Ds(polygon2Ds, true);
+                    if (face2Ds == null || face2Ds.Count == 0)
+                        continue;
+
+                    List<Face3D> face3Ds = new List<Face3D>();
+                    face3Ds.AddRange(face2Ds.ConvertAll(x => new Face3D(plane_Bottom, x)));
+                    face3Ds.AddRange(face2Ds.ConvertAll(x => new Face3D(plane_Top, x)));
+
+                    List<Shell> shells_Temp = Geometry.Spatial.Create.Shells(face3Ds, tolerance);
+                    if (shells_Temp == null || shells_Temp.Count == 0)
+                        continue;
+
+                    result.AddRange(shells_Temp);
+                }
             }
 
-            return shells;
+            return result;
         }
     }
 }

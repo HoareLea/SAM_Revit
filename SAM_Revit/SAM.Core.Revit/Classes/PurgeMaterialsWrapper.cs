495c20fba30ca2a9513bcfaf3379bdcfa8ff0872
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-07-07 13:35:34 +0100

    PurgeMaterials Implementation

---
+++ SAM_Revit/SAM.Core.Revit/Classes/PurgeMaterialsWrapper.cs
@@ -0,0 +1,131 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Core.Revit
+{
+    public class PurgeMaterialsWrapper
+    {
+        private Document document;
+
+        private bool checkElements;
+        private bool purge;
+
+        public PurgeMaterialsWrapper(Document document)
+        {
+            this.document = document;
+        }
+
+        private void Initialize()
+        {
+            checkElements = false;
+            purge = false;
+        }
+
+        public Dictionary<ElementId, string> Purge()
+        {
+            if(document == null)
+            {
+                return null;
+            }
+
+            return Purge(new FilteredElementCollector(document).OfClass(typeof(Material)).ToElementIds()?.ToArray());
+        }
+
+        public Dictionary<ElementId, string>Purge(params ElementId[] elementIds)
+        {
+            if(elementIds == null)
+            {
+                return null;
+            }
+
+            Dictionary<ElementId, string> result = new Dictionary<ElementId, string>();
+
+            if (elementIds.Length == 0)
+            {
+                return result;
+            }
+
+            Application application = document.Application;
+            application.DocumentChanged += Application_DocumentChanged;
+
+            foreach (ElementId elementId in elementIds)
+            {
+                string name = document.GetElement(elementId)?.Name;
+
+                using (TransactionGroup transactionGroup = new TransactionGroup(document, "Purge Materials"))
+                {
+                    transactionGroup.Start();
+                    using (Transaction transaction = new Transaction(document, "Purge Material"))
+                    {
+                        transaction.Start();
+                        checkElements = true;
+                        document.Delete(elementId);
+                        transaction.Commit();
+                    }
+
+                    checkElements = false;
+
+                    if (purge)
+                    {
+                        transactionGroup.Assimilate();
+                        result[elementId] = name;
+                    }
+                    else
+                    {
+                        transactionGroup.RollBack();
+                    }
+                }
+            }
+
+            application.DocumentChanged -= Application_DocumentChanged;
+
+            Initialize();
+
+            return result;
+        }
+
+        public Dictionary<ElementId, string>Purge(params string[] names)
+        {
+            if(names == null || document == null)
+            {
+                return null;
+            }
+
+            List<Element> elements = new FilteredElementCollector(document).OfClass(typeof(Material)).ToList();
+            if(elements == null)
+            {
+                return null;
+            }
+
+            List<ElementId> elementIds = new List<ElementId>();
+            foreach(Element element in elements)
+            {
+                if(names.Contains(element.Name))
+                {
+                    elementIds.Add(element.Id);
+                }
+            }
+
+            return Purge(elementIds.ToArray());
+        }
+
+        private void Application_DocumentChanged(object sender, Autodesk.Revit.DB.Events.DocumentChangedEventArgs e)
+        {
+            purge = false;
+
+            if (!checkElements)
+            {
+                return;
+            }
+
+            ICollection<ElementId> elementIds_Deleted = e.GetDeletedElementIds();
+            ICollection<ElementId> elementIds_Modified = e.GetModifiedElementIds();
+
+            int count = elementIds_Deleted.Count + elementIds_Modified.Count;
+
+            purge = count == 1;
+        }
+    }
+}
---
+++ SAM_Revit/SAM.Core.Revit/Modify/PurgeMaterials.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using System.Collections.Generic;
+
+namespace SAM.Core.Revit
+{
+    public static partial class Modify
+    {
+        public static Dictionary<ElementId, string> PurgeMaterials(this Document document)
+        {
+            if(document == null)
+            {
+                return null;
+            }
+
+            return new PurgeMaterialsWrapper(document).Purge();
+        }
+
+        public static Dictionary<ElementId, string> PurgeMaterials(this Document document, params ElementId[] elementIds)
+        {
+            if (document == null || elementIds == null)
+            {
+                return null;
+            }
+
+            return new PurgeMaterialsWrapper(document).Purge(elementIds);
+        }
+
+        public static Dictionary<ElementId, string> PurgeMaterials(this Document document, params string[] names)
+        {
+            if (document == null || names == null)
+            {
+                return null;
+            }
+
+            return new PurgeMaterialsWrapper(document).Purge(names);
+        }
+
+
+    }
+}

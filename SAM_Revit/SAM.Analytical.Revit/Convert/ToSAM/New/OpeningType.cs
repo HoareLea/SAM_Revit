567aece1d45b8f1ae3e07462caa3c3466ab9458c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-10-28 09:54:36 +0100

    IsTransparent Implemented

--- SAM_Revit/SAM.Analytical.Revit/Convert/ToSAM/New/OpeningType.cs
+++ SAM_Revit/SAM.Analytical.Revit/Convert/ToSAM/New/OpeningType.cs
@@ -1,5 +1,8 @@
 ﻿using Autodesk.Revit.DB;
 using SAM.Core.Revit;
+using SAM.Geometry.Spatial;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SAM.Analytical.Revit
 {
@@ -26,6 +29,65 @@ namespace SAM.Analytical.Revit
                 case BuiltInCategory.OST_Doors:
                     result = new DoorType(name);
                     break;
+            }
+
+            Dictionary<ElementId, List<Shell>> materialElementIdsDictionary = Geometry.Revit.Query.MaterialElementIdsDictionary(familySymbol);
+            if(materialElementIdsDictionary != null && materialElementIdsDictionary.Count != 0)
+            {
+                Document document = familySymbol.Document;
+
+                Material material_Transparent = null;
+                double volume_Transparent = double.MinValue;
+                double thickness_Transparent = 0;
+
+                Material material_Opaque = null;
+                double volume_Opaque = double.MinValue;
+                double thickness_Opaque = 0;
+
+
+                foreach (KeyValuePair<ElementId, List<Shell>> keyValuePair in materialElementIdsDictionary)
+                {
+                    Material material = document.GetElement(keyValuePair.Key) as Material;
+                    if(material == null)
+                    {
+                        continue;
+                    }
+
+                    double volume = keyValuePair.Value.ConvertAll(x => x.Volume()).Sum();
+                    if(Core.Revit.Query.IsTransparent(material))
+                    {
+                        if(volume_Transparent < volume)
+                        {
+                            volume_Transparent = volume;
+                            material_Transparent = material;
+                            thickness_Transparent = Geometry.Spatial.Query.MinDimension(keyValuePair.Value.ConvertAll(x => x.GetBoundingBox()));
+                        }
+                    }
+                    else
+                    {
+                        if (volume_Opaque < volume)
+                        {
+                            volume_Opaque = volume;
+                            material_Opaque = material;
+                            thickness_Opaque = Geometry.Spatial.Query.MinDimension(keyValuePair.Value.ConvertAll(x => x.GetBoundingBox()));
+                        }
+
+                    }
+                }
+
+                if(material_Transparent != null)
+                {
+                    List<Architectural.MaterialLayer> materialLayers = new List<Architectural.MaterialLayer>();
+                    materialLayers.Add(new Architectural.MaterialLayer(material_Transparent.Name, thickness_Transparent));
+                    result.PaneMaterialLayers = materialLayers;
+                }
+
+                if (material_Opaque != null)
+                {
+                    List<Architectural.MaterialLayer> materialLayers = new List<Architectural.MaterialLayer>();
+                    materialLayers.Add(new Architectural.MaterialLayer(material_Opaque.Name, thickness_Opaque));
+                    result.FrameMaterialLayers = materialLayers;
+                }
             }
 
             result.UpdateParameterSets(familySymbol);
---
+++ SAM_Revit/SAM.Core.Revit/Query/IsTransparent.cs
@@ -0,0 +1,15 @@
+﻿namespace SAM.Core.Revit
+{
+    public static partial class Query
+    {
+        public static bool IsTransparent(this Autodesk.Revit.DB.Material material)
+        {
+            if(material == null)
+            {
+                return false;
+            }
+
+            return material.Transparency >= 10;
+        }
+    }
+}

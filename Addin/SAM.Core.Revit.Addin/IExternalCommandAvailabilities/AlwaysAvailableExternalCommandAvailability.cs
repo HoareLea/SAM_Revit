0c4419445dfaba3ba87ce334184520a2ec388cf5
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-03-04 14:17:26 +0000

    ExternalCommandAvailabilities Added

---
+++ Addin/SAM.Core.Revit.Addin/IExternalCommandAvailabilities/AlwaysAvailableExternalCommandAvailability.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace SAM.Core.Revit.Addin
+{
+    public class AlwaysAvailableExternalCommandAvailability : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+        {
+            return true;
+        }
+    }
+}

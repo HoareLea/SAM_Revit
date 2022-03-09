5e6e305fbd1fc235eee201fb4711d71bd0f547bd
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-03-09 10:31:04 +0000

    CreateViews IExternalCommand Updated

--- Addin/SAM.Analytical.Revit.Addin/IExternalCommands/CreateViews.cs
+++ Addin/SAM.Analytical.Revit.Addin/IExternalCommands/CreateViews.cs
@@ -4,6 +4,7 @@ using Autodesk.Revit.UI;
 using SAM.Analytical.Revit.Addin.Properties;
 using SAM.Core.Revit.Addin;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Media.Imaging;
 
 namespace SAM.Analytical.Revit.Addin
@@ -24,12 +25,56 @@ namespace SAM.Analytical.Revit.Addin
                 return Result.Failed;
             }
 
-            string[] templateNames = new string[] { "RiserCLG", "RiserHTG", "RiserVNT", "ICType", "RefExhaust", "RefSupply", "SAM Model", "NoPeople", "Heating Load", "Cooling Load"};
+            List<View> views = new FilteredElementCollector(document).OfClass(typeof(View)).Cast<View>().ToList();
+            if (views == null || views.Count == 0)
+            {
+                return Result.Failed;
+            }
+
+            for(int i=views.Count - 1; i >= 0; i--)
+            {
+                View view = views[i];
+                if(view == null)
+                {
+                    views.RemoveAt(i);
+                    continue;
+                }
+
+                if(view.ViewType != ViewType.FloorPlan)
+                {
+                    views.RemoveAt(i);
+                    continue;
+                }
+
+                if(!view.IsTemplate)
+                {
+                    views.RemoveAt(i);
+                    continue;
+                }
+
+            }
+
+            string[] templateNames = new string[] { "RiserCLG", "RiserHTG", "RiserVNT", "ICType", "RefExhaust", "RefSupply", "SAM Model", "NoPeople", "Heating Load", "Cooling Load" };
+
+            using (Core.Windows.Forms.TreeViewForm<View> treeViewForm = new Core.Windows.Forms.TreeViewForm<View>("Select Templates", views, (View view) => view.Name, null, (View view) => templateNames.Contains(view.Name)))
+            {
+                if(treeViewForm.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                {
+                    return Result.Cancelled;
+                }
+
+                views = treeViewForm.SelectedItems;
+            }
+
+            if(views == null || views.Count == 0)
+            {
+                return Result.Failed;
+            }
 
             using (Transaction transaction = new Transaction(document, "Create Views"))
             {
                 transaction.Start();
-                Core.Revit.Modify.DuplicateViews(document, "00_Reference", templateNames, new ViewType[] { ViewType.FloorPlan });
+                Core.Revit.Modify.DuplicateViews(document, "00_Reference", views.ConvertAll(x => x.Name), new ViewType[] { ViewType.FloorPlan });
                 transaction.Commit();
             }
 

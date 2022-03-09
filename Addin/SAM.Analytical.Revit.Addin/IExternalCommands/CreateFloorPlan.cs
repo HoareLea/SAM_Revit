17843335b0d298c99faa5fced476c5bab3d84f72
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-03-09 13:16:17 +0000

    CreateFloorPlans IExternalCommand Implemented

---
+++ Addin/SAM.Analytical.Revit.Addin/IExternalCommands/CreateFloorPlan.cs
@@ -0,0 +1,63 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using SAM.Analytical.Revit.Addin.Properties;
+using SAM.Core.Revit.Addin;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace SAM.Analytical.Revit.Addin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class CreateFloorPlan : SAMExternalCommand
+    {
+        public override string RibbonPanelName => "Project Setup";
+
+        public override int Index => 9;
+
+        public override Result Execute(ExternalCommandData externalCommandData, ref string message, ElementSet elementSet)
+        {
+            Document document = externalCommandData?.Application?.ActiveUIDocument?.Document;
+            if(document == null)
+            {
+                return Result.Failed;
+            }
+
+            ViewPlan viewPlan = document.GetElement(new ElementId(312)) as ViewPlan;
+            if(viewPlan == null)
+            {
+                MessageBox.Show("Could not find view to be duplicated");
+                return Result.Failed;
+            }
+
+            List<Level> levels = new FilteredElementCollector(document).OfClass(typeof(Level)).Cast<Level>().ToList();
+            if(levels == null || levels.Count == 0)
+            {
+                MessageBox.Show("Could not find levels");
+                return Result.Failed;
+            }
+
+            using (Transaction transaction = new Transaction(document, "Create Views"))
+            {
+                transaction.Start();
+                List<ViewPlan> result = Core.Revit.Modify.DuplicateViewPlan(viewPlan, levels, true);
+                transaction.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+
+        public override void Create(RibbonPanel ribbonPanel)
+        {
+            BitmapSource bitmapSource = Core.Windows.Convert.ToBitmapSource(Resources.SAM_Small);
+
+            PushButton pushButton = ribbonPanel.AddItem(new PushButtonData(Core.Query.FullTypeName(GetType()), "Create\nFloor Plans", GetType().Assembly.Location, GetType().FullName)) as PushButton;
+            pushButton.ToolTip = "Create Floor Plans";
+            pushButton.LargeImage = bitmapSource;
+            pushButton.Image = bitmapSource;
+        }
+    }
+}

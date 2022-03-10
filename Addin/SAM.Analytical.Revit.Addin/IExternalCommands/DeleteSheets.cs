bc4d1c26c4adba73555ba7255076619203880163
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-03-10 10:07:14 +0000

    DeleteSheets and DeleteViews Implemented

---
+++ Addin/SAM.Analytical.Revit.Addin/IExternalCommands/DeleteSheets.cs
@@ -0,0 +1,73 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using SAM.Analytical.Revit.Addin.Properties;
+using SAM.Core.Revit.Addin;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media.Imaging;
+
+namespace SAM.Analytical.Revit.Addin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class DeleteSheets : SAMExternalCommand
+    {
+        public override string RibbonPanelName => "Project Setup";
+
+        public override int Index => 13;
+
+        public override Result Execute(ExternalCommandData externalCommandData, ref string message, ElementSet elementSet)
+        {
+            Document document = externalCommandData?.Application?.ActiveUIDocument?.Document;
+            if(document == null)
+            {
+                return Result.Failed;
+            }
+
+            List<ViewSheet> viewSheets = new FilteredElementCollector(document).OfClass(typeof(ViewSheet)).Cast<ViewSheet>().ToList();
+            if (viewSheets == null || viewSheets.Count == 0)
+            {
+                return Result.Failed;
+            }
+
+            List<int> ids = new List<int>() { 725518, 725533, 802983, 805316, 835480, 1007139, 1008572 };
+
+            using (Core.Windows.Forms.TreeViewForm<ViewSheet> treeViewForm = new Core.Windows.Forms.TreeViewForm<ViewSheet>("Select Sheets", viewSheets, (ViewSheet x) => string.Format("{0} - {1}", x.SheetNumber, x.Name), null, (ViewSheet x) => !ids.Contains(x.Id.IntegerValue)))
+            {
+                if (treeViewForm.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                {
+                    return Result.Cancelled;
+                }
+
+                viewSheets = treeViewForm.SelectedItems;
+            }
+
+            if(viewSheets == null || viewSheets.Count == 0)
+            {
+                return Result.Failed;
+            }
+
+            using (Transaction transaction = new Transaction(document, "Delete Sheets"))
+            {
+                transaction.Start();
+
+                document.Delete(viewSheets.ConvertAll(x => x.Id));
+
+                transaction.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+
+        public override void Create(RibbonPanel ribbonPanel)
+        {
+            BitmapSource bitmapSource = Core.Windows.Convert.ToBitmapSource(Resources.SAM_Small);
+
+            PushButton pushButton = ribbonPanel.AddItem(new PushButtonData(Core.Query.FullTypeName(GetType()), "Delete Sheets", GetType().Assembly.Location, GetType().FullName)) as PushButton;
+            pushButton.ToolTip = "Delete Sheets";
+            pushButton.LargeImage = bitmapSource;
+            pushButton.Image = bitmapSource;
+        }
+    }
+}
---
+++ Addin/SAM.Analytical.Revit.Addin/IExternalCommands/DeleteViews.cs
@@ -0,0 +1,99 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using SAM.Analytical.Revit.Addin.Properties;
+using SAM.Core.Revit.Addin;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media.Imaging;
+
+namespace SAM.Analytical.Revit.Addin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class DeleteViews : SAMExternalCommand
+    {
+        public override string RibbonPanelName => "Project Setup";
+
+        public override int Index => 12;
+
+        public override Result Execute(ExternalCommandData externalCommandData, ref string message, ElementSet elementSet)
+        {
+            Document document = externalCommandData?.Application?.ActiveUIDocument?.Document;
+            if(document == null)
+            {
+                return Result.Failed;
+            }
+
+            List<View> views = new FilteredElementCollector(document).OfClass(typeof(View)).Cast<View>().ToList();
+            if (views == null || views.Count == 0)
+            {
+                return Result.Failed;
+            }
+
+            for (int i = views.Count - 1; i >= 0; i--)
+            {
+                View view = views[i];
+                if (view == null)
+                {
+                    views.RemoveAt(i);
+                    continue;
+                }
+
+                if (view.ViewType != ViewType.FloorPlan)
+                {
+                    views.RemoveAt(i);
+                    continue;
+                }
+
+                if (!view.IsTemplate)
+                {
+                    views.RemoveAt(i);
+                    continue;
+                }
+
+            }
+
+            List<string> templateNames = new List<string>() { "00_Reference"};
+
+            using (Core.Windows.Forms.TreeViewForm<View> treeViewForm = new Core.Windows.Forms.TreeViewForm<View>("Select Templates", views, (View view) => view.Name, null, (View view) => !templateNames.Contains(view.Name)))
+            {
+                if (treeViewForm.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                {
+                    return Result.Cancelled;
+                }
+
+                templateNames = treeViewForm.SelectedItems?.ConvertAll(x => x.Name);
+            }
+
+            if(templateNames == null || templateNames.Count == 0)
+            {
+                return Result.Failed;
+            }
+
+            using (Transaction transaction = new Transaction(document, "Delete Views"))
+            {
+                transaction.Start();
+
+                foreach(string templateName in templateNames)
+                {
+                    List<ElementId> elementIds = Core.Revit.Modify.DeleteViews(document, templateName, false, new ViewType[] { ViewType.FloorPlan });
+                }
+
+                transaction.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+
+        public override void Create(RibbonPanel ribbonPanel)
+        {
+            BitmapSource bitmapSource = Core.Windows.Convert.ToBitmapSource(Resources.SAM_Small);
+
+            PushButton pushButton = ribbonPanel.AddItem(new PushButtonData(Core.Query.FullTypeName(GetType()), "Delete Views", GetType().Assembly.Location, GetType().FullName)) as PushButton;
+            pushButton.ToolTip = "Delete Views";
+            pushButton.LargeImage = bitmapSource;
+            pushButton.Image = bitmapSource;
+        }
+    }
+}

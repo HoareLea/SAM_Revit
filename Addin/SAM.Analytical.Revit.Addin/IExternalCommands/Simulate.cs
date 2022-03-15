b09da3a962c1ac7c6868611f0508c6bf91c3d512
Michal Dengusiak <tassimulation@gmail.com>
2022-03-15 16:38:48 +0000

    Update Simulate.cs

--- Addin/SAM.Analytical.Revit.Addin/IExternalCommands/Simulate.cs
+++ Addin/SAM.Analytical.Revit.Addin/IExternalCommands/Simulate.cs
@@ -32,7 +32,7 @@ namespace SAM.Analytical.Revit.Addin
         public override Result Execute(ExternalCommandData externalCommandData, ref string message, ElementSet elementSet)
         {
             Document document = externalCommandData?.Application?.ActiveUIDocument?.Document;
-            if(document == null)
+            if (document == null)
             {
                 return Result.Failed;
             }
@@ -51,54 +51,63 @@ namespace SAM.Analytical.Revit.Addin
 
                 string path_WeatherData = openFileDialog.FileName;
                 string extension = System.IO.Path.GetExtension(path_WeatherData).ToLower().Trim();
-                if(string.IsNullOrWhiteSpace(extension))
+                if (string.IsNullOrWhiteSpace(extension))
                 {
                     return Result.Failed;
                 }
 
                 try
                 {
-                    if(extension.EndsWith("epw"))
+                    if (extension.EndsWith("epw"))
                     {
                         weatherData = Weather.Convert.ToSAM(path_WeatherData);
                     }
                     else
                     {
                         List<WeatherData> weatherDatas = Weather.Tas.Convert.ToSAM_WeatherDatas(path_WeatherData);
-                        if(weatherDatas == null || weatherDatas.Count == 0)
+                        if (weatherDatas == null || weatherDatas.Count == 0)
                         {
                             return Result.Failed;
                         }
 
-                        weatherDatas.Sort((x, y) => x.Name.CompareTo(y.Name));
-
-                        using (Core.Windows.Forms.ComboBoxForm<WeatherData> comboBoxForm = new Core.Windows.Forms.ComboBoxForm<WeatherData>("Select Weather Data", weatherDatas, (WeatherData x) => x.Name))
+                        if (weatherDatas.Count == 1)
                         {
-                            if(comboBoxForm.ShowDialog() != DialogResult.OK)
+                            weatherData = weatherDatas[0];
+                        }
+                        else
+                        {
+                            weatherDatas.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+                            using (Core.Windows.Forms.ComboBoxForm<WeatherData> comboBoxForm = new Core.Windows.Forms.ComboBoxForm<WeatherData>("Select Weather Data", weatherDatas, (WeatherData x) => x.Name))
                             {
-                                return Result.Cancelled;
+                                if (comboBoxForm.ShowDialog() != DialogResult.OK)
+                                {
+                                    return Result.Cancelled;
+                                }
+
+                                weatherData = comboBoxForm.SelectedItem;
                             }
 
-                            weatherData = comboBoxForm.SelectedItem;
                         }
+
                     }
                 }
-                catch(Exception exception)
+                catch (Exception exception)
                 {
                     weatherData = null;
                 }
             }
 
-            if(weatherData == null)
+            if (weatherData == null)
             {
                 return Result.Failed;
             }
 
-                string path = document.PathName;
-            if(string.IsNullOrWhiteSpace(path))
+            string path = document.PathName;
+            if (string.IsNullOrWhiteSpace(path))
             {
                 string name = document.Title;
-                if(string.IsNullOrWhiteSpace(name))
+                if (string.IsNullOrWhiteSpace(name))
                 {
                     name = "000000_SAM_AnalyticalModel";
                 }
@@ -115,7 +124,7 @@ namespace SAM.Analytical.Revit.Addin
                     path = System.IO.Path.Combine(folderBrowserDialog.SelectedPath, name + ".rvt");
                 }
 
-                if(string.IsNullOrWhiteSpace(path))
+                if (string.IsNullOrWhiteSpace(path))
                 {
                     return Result.Failed;
                 }
@@ -131,14 +140,14 @@ namespace SAM.Analytical.Revit.Addin
                 analyticalModel = Convert.ToSAM_AnalyticalModel(document, new ConvertSettings(true, true, false));
                 transaction.RollBack();
             }
-            
-            if(analyticalModel == null)
+
+            if (analyticalModel == null)
             {
                 return Result.Failed;
             }
 
             string path_gbXML = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), System.IO.Path.GetFileNameWithoutExtension(path));
-            if(System.IO.File.Exists(path_gbXML))
+            if (System.IO.File.Exists(path_gbXML))
             {
                 System.IO.File.Delete(path_gbXML);
             }
@@ -147,7 +156,7 @@ namespace SAM.Analytical.Revit.Addin
 
 
             exported = document.TogbXML(path_gbXML);
-            if(!exported)
+            if (!exported)
             {
                 return Result.Failed;
             }
@@ -164,12 +173,40 @@ namespace SAM.Analytical.Revit.Addin
 
             analyticalModel = Tas.Query.UpdateT3D(analyticalModel, path_T3D);
 
-            //Tas.UpdateWeather Missing !!
-
             string path_TBD = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), System.IO.Path.GetFileNameWithoutExtension(path) + ".tbd");
 
+            using (SAMTBDDocument sAMTBDDocument = new SAMTBDDocument(path_TBD))
+            {
+                TBD.TBDDocument tBDDocument = sAMTBDDocument.TBDDocument;
+
+                Weather.Tas.Modify.UpdateWeatherData(tBDDocument, weatherData);
+
+                double latitude_TBD = Core.Query.Round(analyticalModel.Location.Latitude, 0.01);
+                double longitude_TBD = Core.Query.Round(analyticalModel.Location.Longitude, 0.01);
+
+                double latitude_WeatherData = Core.Query.Round(weatherData.Latitude, 0.01);
+                double longitude_WeatherDate = Core.Query.Round(weatherData.Longitude, 0.01);
+
+                //if (Math.Abs(latitude_TBD - latitude_WeatherData) > 0.01 || Math.Abs(longitude_TBD - longitude_WeatherDate) > 0.01)
+                //{
+                //    AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "WeatherData Longitude or Latitude mismatch");
+                //}
+
+                TBD.Calendar calendar = tBDDocument.Building.GetCalendar();
+
+                TBD.dayType dayType = null;
+
+                dayType = calendar.AddDayType();
+                dayType.name = "HDD";
+
+                dayType = calendar.AddDayType();
+                dayType.name = "CDD";
+
+                sAMTBDDocument.Save();
+            }
+
             exported = Tas.Convert.ToTBD(path_T3D, path_TBD, 1, 365, 15, true);
-            if(!exported)
+            if (!exported)
             {
                 return Result.Failed;
             }
@@ -207,7 +244,7 @@ namespace SAM.Analytical.Revit.Addin
                 hasWeatherData = tBDDocument?.Building.GetWeatherYear() != null;
             }
 
-            if(!hasWeatherData)
+            if (!hasWeatherData)
             {
                 MessageBox.Show("Could not complete simulation. TBD file has no Weather Data");
                 return Result.Failed;
@@ -219,7 +256,7 @@ namespace SAM.Analytical.Revit.Addin
 
 
 
-                adjacencyCluster = analyticalModel.AdjacencyCluster;
+            adjacencyCluster = analyticalModel.AdjacencyCluster;
             Tas.Modify.AddResults(path_TSD, adjacencyCluster);
             analyticalModel = new AnalyticalModel(analyticalModel, adjacencyCluster);
 

7cd5540cc452f32506ce8571d789c902bf43802f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-03-15 16:17:50 +0000

    Simulate IExternalCommand Updated with WeatherData

--- Addin/SAM.Analytical.Revit.Addin/IExternalCommands/Simulate.cs
+++ Addin/SAM.Analytical.Revit.Addin/IExternalCommands/Simulate.cs
@@ -5,6 +5,9 @@ using SAM.Analytical.Revit.Addin.Properties;
 using SAM.Core.Revit;
 using SAM.Core.Revit.Addin;
 using SAM.Core.Tas;
+using SAM.Weather;
+using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Windows.Media.Imaging;
 
@@ -34,7 +37,64 @@ namespace SAM.Analytical.Revit.Addin
                 return Result.Failed;
             }
 
-            string path = document.PathName;
+            WeatherData weatherData = null;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "epw files (*.epw)|*.epw|TAS TBD files (*.tbd)|*.tbd|TAS TSD files (*.tsd)|*.tsd|TAS TWD files (*.twd)|*.twd|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    return Result.Cancelled;
+                }
+
+                string path_WeatherData = openFileDialog.FileName;
+                string extension = System.IO.Path.GetExtension(path_WeatherData).ToLower().Trim();
+                if(string.IsNullOrWhiteSpace(extension))
+                {
+                    return Result.Failed;
+                }
+
+                try
+                {
+                    if(extension.EndsWith("epw"))
+                    {
+                        weatherData = Weather.Convert.ToSAM(path_WeatherData);
+                    }
+                    else
+                    {
+                        List<WeatherData> weatherDatas = Weather.Tas.Convert.ToSAM_WeatherDatas(path_WeatherData);
+                        if(weatherDatas == null || weatherDatas.Count == 0)
+                        {
+                            return Result.Failed;
+                        }
+
+                        weatherDatas.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+                        using (Core.Windows.Forms.ComboBoxForm<WeatherData> comboBoxForm = new Core.Windows.Forms.ComboBoxForm<WeatherData>("Select Weather Data", weatherDatas, (WeatherData x) => x.Name))
+                        {
+                            if(comboBoxForm.ShowDialog() != DialogResult.OK)
+                            {
+                                return Result.Cancelled;
+                            }
+
+                            weatherData = comboBoxForm.SelectedItem;
+                        }
+                    }
+                }
+                catch(Exception exception)
+                {
+                    weatherData = null;
+                }
+            }
+
+            if(weatherData == null)
+            {
+                return Result.Failed;
+            }
+
+                string path = document.PathName;
             if(string.IsNullOrWhiteSpace(path))
             {
                 string name = document.Title;

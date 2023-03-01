54586c9ef09b6bf0d8e15bf54e81f8015d52731d
Ziolkowski <jakubziolkowski@digiproject.uk>
2023-03-01 10:56:23 +0000

    Version Updated

--- Grasshopper/SAM.Analytical.Grasshopper.Revit/Component/RevitSAMAnalyticalByElement.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.Revit/Component/RevitSAMAnalyticalByElement.cs
@@ -20,7 +20,7 @@ namespace SAM.Analytical.Grasshopper.Revit
         /// <summary>
         /// The latest version of this component
         /// </summary>
-        public override string LatestComponentVersion => "1.0.1";
+        public override string LatestComponentVersion => "1.0.2";
 
         /// <summary>
         /// Provides an Icon for the component.
